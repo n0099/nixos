f8c1b1c4d70db23422ab9b68e27a918148ea248d
n0099 <n@n0099.net>
2025-07-10 05:56:09 +0000

    + `n0099.nix`

---
+++ n0099.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+
+{
+  users.users.n0099 = {
+    extraGroups = [ "wheel" "docker" ];
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLshjq9QWQoWa8lDt3wFuWtcaM6o7hj8rF0s9QGedkn" ];
+  };
+  environment.systemPackages = with pkgs; [
+    _7zz
+  ];
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+    nginx.enable = true;
+    vnstat.enable = true;
+  };
+  security = {
+    # https://unix.stackexchange.com/questions/382060/change-default-sudo-password-timeout
+    sudo.configFile = "Defaults timestamp_timeout=15";
+  };
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      defaultEditor = true;
+    };
+    zsh.enable = true;
+    git.enable = true;
+    atop = {
+      enable = true;
+      atopService.enable = false;
+      settings.interval = 1;
+    };
+  };
+  virtualisation.docker = {
+    enable = true;
+    package = pkgs.docker_28;
+  };
+}
