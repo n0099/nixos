1df270580e7940089bc72db6ac15b214874c25ed
n0099 <n@n0099.net>
2025-07-18 11:20:06 +0000

    * add more vim plugins * move plugins depends on `nvim-treesitter` into a inlined module with `lib.mkMerge`q * using colorscheme `desert` instead of `default` @ nixvim.nix

--- nixvim.nix
+++ nixvim.nix
@@ -1,20 +1,44 @@
-{ inputs, ... }:
+{ lib, pkgs, inputs, ... }:
 
 {
   imports = [ inputs.nixvim.nixosModules.nixvim ];
-  programs.nixvim = {
+  programs.nixvim = lib.mkMerge [{
     enable = true;
     viAlias = true;
     vimAlias = true;
     defaultEditor = true;
+    colorscheme = "desert";
     plugins = {
       # leap.enable = true;
       trim.enable = true;
       lastplace.enable = true;
+      visual-whitespace.enable = true;
+      guess-indent.enable = true;
     };
+    extraPlugins = with pkgs; [
+      vimPlugins.vim-better-whitespace
+      vimPlugins.vim-pasta
+      (vimUtils.buildVimPlugin {
+        name = "indentmini";
+        src = fetchFromGitHub {
+          owner = "nvimdev";
+          repo = "indentmini.nvim";
+          rev = "0dc4bc2b3fc763420793e748b672292bc43ee722";
+          hash = "sha256-iMQn9eJuwThatTg9aTKhgHQaBc1NV4h/6gGt+fhZG9k=";
+        };
+      })
+      (vimUtils.buildVimPlugin {
+        name = "auto-indent";
+        src = fetchFromGitHub {
+          owner = "VidocqH";
+          repo = "auto-indent.nvim";
+          rev = "46801cf8857d42a20a73c40b0a5d3dfe8b2b6192";
+          hash = "sha256-dubpVupLfc81Jvb4woSQ63n2+VsJCRjnvDzkFTQE2MQ=";
+        };
+      })
+    ];
     files = {
-      # https://github.com/nix-community/nixvim/issues/2418#issuecomment-2413714276i
-      "after/ftplugin/nix.lua" = {
+      "after/ftplugin/nix.lua" = { # https://github.com/nix-community/nixvim/issues/2418#issuecomment-2413714276
         localOpts = { # https://github.com/nix-community/nixvim/pull/3535#discussion_r2191072494
           tabstop = 2;
           shiftwidth = 2;
@@ -28,5 +52,30 @@
       shiftwidth = 4;
       expandtab = true;
     };
-  };
+  } {
+    plugins = {
+      treesitter = {
+        enable = true;
+        settings.indent.enable = true;
+      };
+      rainbow-delimiters.enable = true;
+      dropbar.enable = true;
+    };
+    extraConfigLua = ''
+      -- `vim.treesitter.start()` will enable highlight of `nvim-treesitter` and `rainbow-delimiters`
+      -- or highlights of `rainbow-delimiters` will show on the first buffer update
+      -- https://neovim.io/doc/user/treesitter.html#vim.treesitter.start()
+      -- https://neovim.io/doc/user/treesitter.html#vim.treesitter.language.add()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(args.match)
+          if vim.treesitter.language.add(lang) then
+            vim.treesitter.start(args.buf, lang)
+          end
+        end
+      })
+      -- https://github.com/nvimdev/indentmini.nvim/blob/0dc4bc2b3fc763420793e748b672292bc43ee722/README.md#config
+      require("indentmini").setup({ only_current = true })
+    '';
+  }];
 }
