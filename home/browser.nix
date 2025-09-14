c8a01f97a8272da9967379a6a6b70136f499a535
n0099 <n@n0099.net>
2025-09-14 05:28:58 +0000

    * using firefox policy to set and lock some preferences @ home/browser.nix

--- home/browser.nix
+++ home/browser.nix
@@ -2,6 +2,12 @@
 
 let
   addons = pkgs.nur.repos.rycee.firefox-addons;
+  mkPreferences = lib.mapAttrs (
+    _: value: {
+      Value = value;
+      Status = "locked";
+    }
+  );
 in
 {
   programs.librewolf = lib.mkMerge [
@@ -22,6 +28,38 @@ in
         clearurls
         promnesia
       ];
+      policies = {
+        DisplayBookmarksToolbar = "newtab";
+        Homepage.StartPage = "previous-session";
+        SanitizeOnShutdown = {
+          Locked = true;
+          Cache = false;
+          Cookies = false;
+        };
+        Preferences = mkPreferences (
+          # about:policies
+          # https://mozilla.github.io/policy-templates
+          # https://github.com/mozilla/policy-templates
+          # https://github.com/NixOS/nixpkgs/blob/b4c2c57c31e68544982226d07e4719a2d86302a8/nixos/modules/programs/firefox.nix#L340-L345
+          {
+            "sidebar.revamp" = true;
+            "sidebar.verticalTabs" = true;
+          }
+          // {
+            "browser.urlbar.trimURLs" = false;
+          }
+          // {
+            "browser.newtabpage.activity-stream.feeds.topsites" = true;
+            "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
+            "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = true;
+            "browser.newtabpage.activity-stream.section.highlights.includeVisited" = true;
+            "browser.newtabpage.activity-stream.feeds.section.highlights" = true;
+            "browser.newtabpage.activity-stream.section.highlights.rows" = 4;
+            "browser.newtabpage.activity-stream.topSitesRows" = 4;
+            "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
+          }
+        );
+      };
     }
   ];
 }
