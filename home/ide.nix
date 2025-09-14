235fbc83243b3cea0a55617e9d1bfb8523e71db7
n0099 <n@n0099.net>
2025-09-14 06:11:33 +0000

    + `home/ide.nix` to install vscodium and unfree products from jetbrains then import it in `n0099.nix`

---
+++ home/ide.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  home.packages =
+    let
+      # https://nixos.org/manual/nixpkgs/stable/#sec-allow-unfree
+      # https://discourse.nixos.org/t/1000-instances-of-nixpkgs/17347
+      pkgs-unfree = import inputs.nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfreePredicate =
+          pkg:
+          lib.elem (lib.getName pkg) [
+            "idea-ultimate"
+            "datagrip"
+            "phpstorm"
+            "rider"
+          ];
+      };
+    in
+    with pkgs-unfree.jetbrains;
+    [
+      idea-ultimate
+      datagrip
+      phpstorm
+      rider
+    ];
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+  };
+}
--- home/n0099.nix
+++ home/n0099.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./ide.nix
     ./browser.nix
   ];
   home.packages =
