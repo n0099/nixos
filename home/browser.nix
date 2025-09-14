8ce5fc71b7cf720637f168b41fdf4ccda2015850
n0099 <n@n0099.net>
2025-09-14 05:45:49 +0000

    * install addons `ublock-origin` & `kiss-translator` and set initial settings for them following https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/26 @ home/browser.nix

--- home/browser.nix
+++ home/browser.nix
@@ -61,5 +61,131 @@ in
         );
       };
     }
+    {
+      profiles.default.extensions.packages = [ addons.ublock-origin ];
+      # https://mozilla.github.io/policy-templates/#3rdparty
+      # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/26
+      # click `Reset to default settings…` in moz-extension://95bd60a3-bbff-4694-a503-b7f8d4a3e9e6/dashboard.html#settings.html is required after managed storage changed
+      policies."3rdparty".Extensions."uBlock0@raymondhill.net".adminSettings = {
+        userSettings = {
+          suspendUntilListsAreLoaded = true;
+          advancedUserEnabled = true;
+        };
+        selectedFilterLists = [
+          "user-filters"
+        ]
+        ++ [
+          "ublock-filters"
+          "ublock-badware"
+          "ublock-privacy"
+          "ublock-quick-fixes"
+          "ublock-unbreak"
+          "ublock-experimental"
+        ]
+        ++ [
+          "easylist"
+          "adguard-generic"
+          "adguard-mobile"
+        ]
+        ++ [
+          "easyprivacy"
+          "LegitimateURLShortener"
+          "adguard-spyware-url"
+          # "block-lan"
+        ]
+        ++ [
+          "urlhaus-1"
+          "curben-phishing"
+        ]
+        ++ [
+          "plowe-0"
+          "dpollock-0"
+        ]
+        ++ [
+          "fanboy-cookiemonster"
+          "ublock-cookies-easylist"
+        ]
+        ++ [
+          "adguard-cookies"
+          "ublock-cookies-adguard"
+        ]
+        ++ [
+          "fanboy-social"
+          "adguard-social"
+          "fanboy-thirdparty_social"
+        ]
+        ++ [
+          "easylist-chat"
+          "easylist-newsletters"
+          "easylist-notifications"
+          "easylist-annoyances"
+          "adguard-mobile-app-banners"
+          "adguard-other-annoyances"
+          "adguard-popup-overlays"
+          "adguard-widgets"
+          "ublock-annoyances"
+        ]
+        ++ [
+          "ALB-0"
+          "BGR-0"
+          "CHN-0"
+          "CZE-0"
+          "DEU-0"
+          "EST-0"
+          "ara-0"
+          "spa-1"
+          "spa-0"
+          "FIN-0"
+          "FRA-0"
+          "GRC-0"
+          "HRV-0"
+          "HUN-0"
+          "IDN-0"
+          "ISR-0"
+          "IND-0"
+          "IRN-0"
+          "ISL-0"
+          "ITA-0"
+          "JPN-1"
+          "KOR-1"
+          "LTU-0"
+          "LVA-0"
+          "MKD-0"
+          "NLD-0"
+          "NOR-0"
+          "POL-0"
+          "ROU-1"
+        ]
+        ++ [
+          "RUS-0"
+          "RUS-1"
+        ]
+        ++ [
+          "SWE-1"
+          "SVN-0"
+          "THA-0"
+          "TUR-0"
+          "UKR-0"
+          "VIE-1"
+        ];
+      };
+    }
+    {
+    {
+      profiles.default = {
+        extensions.packages = [ addons.kiss-translator ];
+        userContent = ''
+          kiss-translator {
+            display: inline-block;
+            outline: dashed darkgray 1px;
+            padding: 0.25rem;
+            font-language-override: "ZHS";
+          }
+          kiss-translator > br {
+            display: none;
+          }
+        '';
+      };
+    }
   ];
 }
