ae985c77376be0e5a9be6b7f1f9f66326c112aba
n0099 <n@n0099.net>
2025-09-14 06:13:17 +0000

    * install unfree package `osu-lazer-bin` @ home/n0099.nix

--- home/n0099.nix
+++ home/n0099.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
@@ -12,5 +12,16 @@
       remmina
       kdePackages.krdc
       kdePackages.krfb
-    ];
+    ]
+    ++ (
+      let
+        # https://nixos.org/manual/nixpkgs/stable/#sec-allow-unfree
+        # https://discourse.nixos.org/t/1000-instances-of-nixpkgs/17347
+        pkgs-unfree = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfreePredicate = pkg: lib.elem (lib.getName pkg) [ "osu-lazer-bin" ];
+        };
+      in
+      [ pkgs-unfree.osu-lazer-bin ]
+    );
 }
