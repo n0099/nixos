13806237de71bc351ef34eff37af6c52b3c60a6e
n0099 <n@n0099.net>
2025-09-14 04:47:42 +0000

    + `flake.nix` and `.gitattributes` for `flake.lock`

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    base.url = "./base";
+    nur = {
+      url = "github:nix-community/NUR";
+      # https://discourse.nixos.org/t/flake-how-make-nixpkgs-self-follow-another-inputs-nixpkgs/10867
+      inputs.nixpkgs.follows = "base/nixpkgs";
+    };
+  };
+  outputs =
+    {
+      base,
+      nur,
+      ...
+    }@inputs:
+    (base.outputs.withModules ({ inherit nur; } // inputs) {
+      nixos = [
+        nur.modules.nixos.default # https://nur.nix-community.org/documentation/#using-the-flake-in-nixos
+        ./configuration.nix
+      ];
+    });
+}
