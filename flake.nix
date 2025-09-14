1c14bb19f1a60ba6b4355e096d272f2812c39034
n0099 <n@n0099.net>
2025-09-14 07:27:41 +0000

    * move root level `nixConfig.substituters` into module as `nix.settings.substituters` to persistence it in `nix.conf` @ flake.nix $ git submodule update --remote

--- flake.nix
+++ flake.nix
@@ -1,22 +1,9 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      # https://discourse.nixos.org/t/enable-different-priority-substituters-with-flake/56073/7
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/"
-      "https://cache.nixos.org"
-      # https://nix-community.org/cache/
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
   inputs = {
     base.url = "./base";
     nur = {
       url = "github:nix-community/NUR";
-      # https://discourse.nixos.org/t/flake-how-make-nixpkgs-self-follow-another-inputs-nixpkgs/10867
-      inputs.nixpkgs.follows = "base/nixpkgs";
+      inputs.nixpkgs.follows = "base/nixpkgs"; # https://discourse.nixos.org/t/flake-how-make-nixpkgs-self-follow-another-inputs-nixpkgs/10867
     };
   };
   outputs =
@@ -27,6 +14,13 @@
     }@inputs:
     (base.outputs.withModules ({ inherit nur; } // inputs) {
       nixos = [
+        {
+          nix.settings = {
+            # https://nix-community.org/cache/
+            substituters = [ "https://nix-community.cachix.org" ];
+            trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+          };
+        }
         nur.modules.nixos.default # https://nur.nix-community.org/documentation/#using-the-flake-in-nixos
         ./configuration.nix
         ./desktop.nix
