512287b500d9b227f9d50fc71ec6a658bc3d911d
n0099 <n@n0099.net>
2025-09-19 15:03:18 +0000

    + font https://github.com/unicode-org/last-resort-font * wrapping `fontconfig.localConf` with its xml structure * add SC variants of adobe source han fonts to 3 built-in families like https://gist.github.com/akiirui/b3f36e8bdf9a9f5636a98113960bc7f4 to fix https://japanese.stackexchange.com/questions/86411/why-are-%E5%85%B3-and-%E5%A4%8D-half-width-in-japanese @ desktop.nix

--- desktop.nix
+++ desktop.nix
@@ -51,14 +51,21 @@
       networking.networkmanager.enable = true;
     }
     {
-      fonts = {
-        packages = with pkgs; [
-          nerd-fonts.fira-code
-          source-han-sans-vf-otf
-        ];
-        fontconfig = {
-          enable = true;
-          localConf = ''
+      fonts = lib.mkMerge [
+        {
+          packages = with pkgs; [
+            nerd-fonts.fira-code
+            last-resort
+          ];
+          fontconfig.enable = true;
+          fontconfig.localConf = ''
+            <?xml version="1.0"?>
+            <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+            <fontconfig>
+          '';
+        }
+        {
+          fontconfig.localConf = ''
             <match target="font">
               <!-- https://discuss.kde.org/t/konsole-renders-btop-characters-not-properly/12502 -->
               <!-- https://stackoverflow.com/questions/47501411/how-to-set-font-per-unicode-range-codepoint-in-fontconfig/47614324#47614324 -->
@@ -80,8 +87,51 @@
               </edit>
             </match>
           '';
-        };
-      };
+        }
+        {
+          packages = with pkgs; [
+            # source-sans
+            source-han-sans
+            # source-han-sans-vf-otf
+            # source-serif
+            source-han-serif
+            # source-han-serif-vf-otf
+            # source-code-pro
+            source-han-mono
+          ];
+          fontconfig.localConf = ''
+            <!-- https://gist.github.com/akiirui/b3f36e8bdf9a9f5636a98113960bc7f4 -->
+            <!-- https://japanese.stackexchange.com/questions/86411/why-are-%E5%85%B3-and-%E5%A4%8D-half-width-in-japanese -->
+            <match target="pattern">
+              <test name="family">
+                <string>serif</string>
+              </test>
+              <edit name="family" mode="append" binding="strong">
+                <string>Source Han Serif SC</string>
+              </edit>
+            </match>
+            <match target="pattern">
+              <test name="family">
+                <string>sans-serif</string>
+              </test>
+              <edit name="family" mode="append" binding="strong">
+                <string>Source Han Sans SC</string>
+              </edit>
+            </match>
+            <match target="pattern">
+              <test name="family">
+                <string>monospace</string>
+              </test>
+              <edit name="family" mode="append" binding="strong">
+                <string>Source Han Mono SC</string>
+              </edit>
+            </match>
+          '';
+        }
+        {
+          fontconfig.localConf = "</fontconfig>";
+        }
+      ];
     }
   ];
 }
