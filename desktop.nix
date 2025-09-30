2bd7cc74ca366fed4321150d3188202e69444dc1
n0099 <n@n0099.net>
2025-09-30 17:52:53 +0000

    * remove the unnecessary root-level key `config` with `mkMerge` and without any other keys @ desktop.nix & home/kde.nix

--- desktop.nix
+++ desktop.nix
@@ -5,190 +5,188 @@
   ...
 }:
 
-{
-  config = lib.mkMerge [
-    {
-      services = {
-        xserver.enable = true;
-        displayManager.sddm.enable = true;
-        desktopManager.plasma6.enable = true;
-      };
-    }
-    {
-      boot = {
-        kernelPackages = lib.mkForce pkgs.linuxPackages_lqx;
-        kernelPatches = [
-          {
-            name = "PREEMPT_RT";
-            patch = null;
-            structuredExtraConfig = {
-              PREEMPT_RT = lib.kernel.yes;
-              PREEMPT_VOLUNTARY = lib.mkForce lib.kernel.unset; # https://github.com/NixOS/nixpkgs/blob/d2ed99647a4b195f0bcc440f76edfa10aeb3b743/pkgs/os-specific/linux/kernel/common-config.nix#L1304
-            }
-            // lib.genAttrs [
-              # https://realtime-linux.org/getting-started-with-preempt_rt-guide/
-              # https://www.osadl.org/fileadmin/dam/presentations/COOL-03-2023/COOL-2023-03_Configuration-of-the-Linux-PREEMPT_RT-Kernel_Alexander-Baehr.pdf
-              # "DEBUG_LOCKDEP"
-              "DEBUG_PREEMPT"
-              "DEBUG_OBJECTS"
-              "SLUB_DEBUG"
-            ] (_: lib.kernel.no);
+lib.mkMerge [
+  {
+    services = {
+      xserver.enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma6.enable = true;
+    };
+  }
+  {
+    boot = {
+      kernelPackages = lib.mkForce pkgs.linuxPackages_lqx;
+      kernelPatches = [
+        {
+          name = "PREEMPT_RT";
+          patch = null;
+          structuredExtraConfig = {
+            PREEMPT_RT = lib.kernel.yes;
+            PREEMPT_VOLUNTARY = lib.mkForce lib.kernel.unset; # https://github.com/NixOS/nixpkgs/blob/d2ed99647a4b195f0bcc440f76edfa10aeb3b743/pkgs/os-specific/linux/kernel/common-config.nix#L1304
           }
-          {
-            name = "DRM_I915_GVT";
-            patch = null;
-            structuredExtraConfig = lib.genAttrs [ "DRM_I915_GVT" "DRM_I915_GVT_KVMGT" ] (
-              # https://wiki.nixos.org/wiki/Linux_kernel#Custom_configuration
-              _: lib.mkForce lib.kernel.unset
-            );
-          }
-        ];
-      };
-    }
-    {
-      nixpkgs.config.allowUnfreePredicate =
-        pkg:
-        lib.elem (lib.getName pkg) [
-          "nvidia-x11"
-          "nvidia-settings"
-        ];
-      services.xserver.videoDrivers = [ "nvidia" ];
-      hardware.nvidia = {
-        open = true;
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          # https://github.com/NixOS/nixpkgs/issues/416023#issuecomment-3109657947
-          # $ version=xxx.xx.xx sh -xc 'nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run && echo open-gpu-kernel-modules nvidia-{settings,persistenced} | tr " " "\n" | xargs -I{} , nix-prefetch-github NVIDIA {} --rev $version'
-          version = "580.82.09";
-          sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
-          openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
-          settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
-          persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
-          patchesOpen = [
-            (pkgs.writeText "nvidia-patch"
-              # https://forums.developer.nvidia.com/t/inquiries-regarding-nvidias-support-for-preempt-rt/283007
-              ''
-                diff --git a/kernel-open/conftest.sh b/kernel-open/conftest.sh
-                index 7de4f4af..e3d65c9a 100755
-                --- a/kernel-open/conftest.sh
-                +++ b/kernel-open/conftest.sh
-                @@ -5057,10 +5057,7 @@ case "$5" in
-                         #
-                         VERBOSE=$6
-                -
-                -        if [ -n "$IGNORE_PREEMPT_RT_PRESENCE" ]; then
-                -            exit 0
-                -        fi
-                +        exit 0
-
-                         if test_configuration_option CONFIG_PREEMPT_RT; then
-                             PREEMPT_RT_PRESENT=1
-                         elif test_configuration_option CONFIG_PREEMPT_RT_FULL; then
-              ''
-            )
-          ];
-        };
-        powerManagement.enable = true; # https://wiki.nixos.org/wiki/NVIDIA#Graphical_corruption_and_system_crashes_on_suspend/resume
-      };
-    }
-    {
-      security.rtkit.enable = true;
-      services = {
-        pulseaudio.enable = false;
-        pipewire = {
-          enable = true;
-          pulse.enable = true;
-          alsa = {
-            enable = true;
-            support32Bit = true;
-          };
-        };
-      };
-    }
-    {
-      networking.networkmanager.enable = true;
-    }
-    {
-      fonts = lib.mkMerge [
-        {
-          packages = with pkgs; [
-            nerd-fonts.fira-code
-            last-resort
-          ];
-          fontconfig.enable = true;
-          fontconfig.localConf = ''
-            <?xml version="1.0"?>
-            <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-            <fontconfig>
-          '';
+          // lib.genAttrs [
+            # https://realtime-linux.org/getting-started-with-preempt_rt-guide/
+            # https://www.osadl.org/fileadmin/dam/presentations/COOL-03-2023/COOL-2023-03_Configuration-of-the-Linux-PREEMPT_RT-Kernel_Alexander-Baehr.pdf
+            # "DEBUG_LOCKDEP"
+            "DEBUG_PREEMPT"
+            "DEBUG_OBJECTS"
+            "SLUB_DEBUG"
+          ] (_: lib.kernel.no);
         }
         {
-          fontconfig.localConf = ''
-            <match target="font">
-              <!-- https://discuss.kde.org/t/konsole-renders-btop-characters-not-properly/12502 -->
-              <!-- https://stackoverflow.com/questions/47501411/how-to-set-font-per-unicode-range-codepoint-in-fontconfig/47614324#47614324 -->
-              <!-- https://github.com/cjbassi/ytop/issues/79#issuecomment-676070893 -->
-              <test name="family" compare="eq">
-                <string>FreeMono</string>
-              </test>
-              <edit name="charset" mode="assign">
-                <minus>
-                  <name>charset</name>
-                  <charset>
-                    <range>
-                      <!-- https://codepoints.net/braille_patterns -->
-                      <int>0x2800</int>
-                      <int>0x28FF</int>
-                    </range>
-                  </charset>
-                </minus>
-              </edit>
-            </match>
-          '';
-        }
-        {
-          packages = with pkgs; [
-            # source-sans
-            source-han-sans
-            # source-han-sans-vf-otf
-            # source-serif
-            source-han-serif
-            # source-han-serif-vf-otf
-            # source-code-pro
-            source-han-mono
-          ];
-          fontconfig.localConf = ''
-            <!-- https://gist.github.com/akiirui/b3f36e8bdf9a9f5636a98113960bc7f4 -->
-            <!-- https://japanese.stackexchange.com/questions/86411/why-are-%E5%85%B3-and-%E5%A4%8D-half-width-in-japanese -->
-            <match target="pattern">
-              <test name="family">
-                <string>serif</string>
-              </test>
-              <edit name="family" mode="append" binding="strong">
-                <string>Source Han Serif SC</string>
-              </edit>
-            </match>
-            <match target="pattern">
-              <test name="family">
-                <string>sans-serif</string>
-              </test>
-              <edit name="family" mode="append" binding="strong">
-                <string>Source Han Sans SC</string>
-              </edit>
-            </match>
-            <match target="pattern">
-              <test name="family">
-                <string>monospace</string>
-              </test>
-              <edit name="family" mode="append" binding="strong">
-                <string>Source Han Mono SC</string>
-              </edit>
-            </match>
-          '';
-        }
-        {
-          fontconfig.localConf = "</fontconfig>";
+          name = "DRM_I915_GVT";
+          patch = null;
+          structuredExtraConfig = lib.genAttrs [ "DRM_I915_GVT" "DRM_I915_GVT_KVMGT" ] (
+            # https://wiki.nixos.org/wiki/Linux_kernel#Custom_configuration
+            _: lib.mkForce lib.kernel.unset
+          );
         }
       ];
-    }
-  ];
-}
+    };
+  }
+  {
+    nixpkgs.config.allowUnfreePredicate =
+      pkg:
+      lib.elem (lib.getName pkg) [
+        "nvidia-x11"
+        "nvidia-settings"
+      ];
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+      open = true;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        # https://github.com/NixOS/nixpkgs/issues/416023#issuecomment-3109657947
+        # $ version=xxx.xx.xx sh -xc 'nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run && echo open-gpu-kernel-modules nvidia-{settings,persistenced} | tr " " "\n" | xargs -I{} , nix-prefetch-github NVIDIA {} --rev $version'
+        version = "580.82.09";
+        sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+        openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+        settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+        persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+        patchesOpen = [
+          (pkgs.writeText "nvidia-patch"
+            # https://forums.developer.nvidia.com/t/inquiries-regarding-nvidias-support-for-preempt-rt/283007
+            ''
+              diff --git a/kernel-open/conftest.sh b/kernel-open/conftest.sh
+              index 7de4f4af..e3d65c9a 100755
+              --- a/kernel-open/conftest.sh
+              +++ b/kernel-open/conftest.sh
+              @@ -5057,10 +5057,7 @@ case "$5" in
+                       #
+                       VERBOSE=$6
+              -
+              -        if [ -n "$IGNORE_PREEMPT_RT_PRESENCE" ]; then
+              -            exit 0
+              -        fi
+              +        exit 0
+
+                       if test_configuration_option CONFIG_PREEMPT_RT; then
+                           PREEMPT_RT_PRESENT=1
+                       elif test_configuration_option CONFIG_PREEMPT_RT_FULL; then
+            ''
+          )
+        ];
+      };
+      powerManagement.enable = true; # https://wiki.nixos.org/wiki/NVIDIA#Graphical_corruption_and_system_crashes_on_suspend/resume
+    };
+  }
+  {
+    security.rtkit.enable = true;
+    services = {
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        pulse.enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+      };
+    };
+  }
+  {
+    networking.networkmanager.enable = true;
+  }
+  {
+    fonts = lib.mkMerge [
+      {
+        packages = with pkgs; [
+          nerd-fonts.fira-code
+          last-resort
+        ];
+        fontconfig.enable = true;
+        fontconfig.localConf = ''
+          <?xml version="1.0"?>
+          <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+          <fontconfig>
+        '';
+      }
+      {
+        fontconfig.localConf = ''
+          <match target="font">
+            <!-- https://discuss.kde.org/t/konsole-renders-btop-characters-not-properly/12502 -->
+            <!-- https://stackoverflow.com/questions/47501411/how-to-set-font-per-unicode-range-codepoint-in-fontconfig/47614324#47614324 -->
+            <!-- https://github.com/cjbassi/ytop/issues/79#issuecomment-676070893 -->
+            <test name="family" compare="eq">
+              <string>FreeMono</string>
+            </test>
+            <edit name="charset" mode="assign">
+              <minus>
+                <name>charset</name>
+                <charset>
+                  <range>
+                    <!-- https://codepoints.net/braille_patterns -->
+                    <int>0x2800</int>
+                    <int>0x28FF</int>
+                  </range>
+                </charset>
+              </minus>
+            </edit>
+          </match>
+        '';
+      }
+      {
+        packages = with pkgs; [
+          # source-sans
+          source-han-sans
+          # source-han-sans-vf-otf
+          # source-serif
+          source-han-serif
+          # source-han-serif-vf-otf
+          # source-code-pro
+          source-han-mono
+        ];
+        fontconfig.localConf = ''
+          <!-- https://gist.github.com/akiirui/b3f36e8bdf9a9f5636a98113960bc7f4 -->
+          <!-- https://japanese.stackexchange.com/questions/86411/why-are-%E5%85%B3-and-%E5%A4%8D-half-width-in-japanese -->
+          <match target="pattern">
+            <test name="family">
+              <string>serif</string>
+            </test>
+            <edit name="family" mode="append" binding="strong">
+              <string>Source Han Serif SC</string>
+            </edit>
+          </match>
+          <match target="pattern">
+            <test name="family">
+              <string>sans-serif</string>
+            </test>
+            <edit name="family" mode="append" binding="strong">
+              <string>Source Han Sans SC</string>
+            </edit>
+          </match>
+          <match target="pattern">
+            <test name="family">
+              <string>monospace</string>
+            </test>
+            <edit name="family" mode="append" binding="strong">
+              <string>Source Han Mono SC</string>
+            </edit>
+          </match>
+        '';
+      }
+      {
+        fontconfig.localConf = "</fontconfig>";
+      }
+    ];
+  }
+]
--- home/kde.nix
+++ home/kde.nix
@@ -1,129 +1,127 @@
 { lib, pkgs, ... }:
 
-{
-  config = lib.mkMerge [
-    {
-      programs.plasma = lib.mkMerge [
-        # run `~/.local/share/plasma-manager/run_all.sh` after `nixos-rebuild switch` to restart plasma-shell and apply settings
-        {
-          enable = true;
-          overrideConfig = true;
-        }
-        {
-          # https://danbooru.donmai.us/posts?tags=minuo+ratio%3A16%3A9
-          workspace = {
-            wallpaperFillMode = "preserveAspectFit";
-            wallpaper = (
-              (pkgs.fetchurl {
-                url = "https://cdn.donmai.us/original/cb/bf/__doremy_sweet_and_kana_anaberal_touhou_drawn_by_minuo__cbbf70a927d77d9630c1cb00fe4ca300.png";
-                hash = "sha256-V4Lde1mL7Knq1PCqBdTGbQYPdutaDeNeGqHtB7P1b8g=";
-              }).outPath
-            );
-          };
-          kscreenlocker.appearance.wallpaper =
+lib.mkMerge [
+  {
+    programs.plasma = lib.mkMerge [
+      # run `~/.local/share/plasma-manager/run_all.sh` after `nixos-rebuild switch` to restart plasma-shell and apply settings
+      {
+        enable = true;
+        overrideConfig = true;
+      }
+      {
+        # https://danbooru.donmai.us/posts?tags=minuo+ratio%3A16%3A9
+        workspace = {
+          wallpaperFillMode = "preserveAspectFit";
+          wallpaper = (
             (pkgs.fetchurl {
-              url = "https://cdn.donmai.us/original/06/03/__patchouli_knowledge_koakuma_kaku_seiga_hieda_no_akyuu_and_motoori_kosuzu_touhou_drawn_by_minuo__0603e0e274622ad12b048db4470f5bb4.jpg";
-              hash = "sha256-6DqmS10XBjqWFPE+bJD4ZJSWSRQdXNfQDw+tb1zqofo=";
-            }).outPath;
-        }
-        {
-          panels = [
-            {
-              floating = true;
-              lengthMode = "fit";
-              opacity = "translucent";
-              widgets = [
-                # https://github.com/nix-community/plasma-manager/tree/d47428e5390d6a5a8f764808a4db15929347cd77/modules/widgets
-                { kickoff.icon = "nix-snowflake"; } # https://discuss.kde.org/t/need-help-to-find-the-names-locations-of-certain-icons/33988
-                { pager = { }; }
-                {
-                  iconTasks = {
-                    iconsOnly = false; # https://github.com/nix-community/plasma-manager/issues/473#issuecomment-2708983491
-                    launchers = [
-                      # https://develop.kde.org/docs/plasma/scripting/keys/
-                      "preferred://filemanager"
-                      "preferred://browser"
-                      "applications:org.kde.konsole.desktop"
-                    ];
-                  };
-                }
-                "org.kde.plasma.marginsseparator"
-                { systemTray = { }; }
-                {
-                  digitalClock = {
-                    date.format = "isoDate";
-                    time = {
-                      format = "24h";
-                      showSeconds = "always";
-                    };
-                    calendar = {
-                      firstDayOfWeek = "monday";
-                      showWeekNumbers = true;
-                    };
-                  };
-                }
-                "org.kde.plasma.showdesktop"
-              ];
-            }
-          ];
-        }
-      ];
-    }
-    {
-      programs.plasma.workspace.splashScreen.theme = "minecraftworldloading-kde-splash";
-      xdg.dataFile."plasma/look-and-feel/minecraftworldloading-kde-splash" = {
-        enable = true;
-        source =
-          (pkgs.fetchFromGitHub {
-            owner = "Samsu-F";
-            repo = "minecraftworldloading-kde-splash";
-            rev = "db3dcf5751afe795d92eec80ff83a16222ca2a18";
-            hash = "sha256-XqzjrHjBDV1xTWQjZ0A4MAu6BlHqlEPNkk/48PjBZEI=";
+              url = "https://cdn.donmai.us/original/cb/bf/__doremy_sweet_and_kana_anaberal_touhou_drawn_by_minuo__cbbf70a927d77d9630c1cb00fe4ca300.png";
+              hash = "sha256-V4Lde1mL7Knq1PCqBdTGbQYPdutaDeNeGqHtB7P1b8g=";
+            }).outPath
+          );
+        };
+        kscreenlocker.appearance.wallpaper =
+          (pkgs.fetchurl {
+            url = "https://cdn.donmai.us/original/06/03/__patchouli_knowledge_koakuma_kaku_seiga_hieda_no_akyuu_and_motoori_kosuzu_touhou_drawn_by_minuo__0603e0e274622ad12b048db4470f5bb4.jpg";
+            hash = "sha256-6DqmS10XBjqWFPE+bJD4ZJSWSRQdXNfQDw+tb1zqofo=";
           }).outPath;
-      };
-    }
-    {
-      programs.konsole = {
-        enable = true;
-        defaultProfile = "default";
-        profiles.default = {
-          font = {
-            name = "FiraCode Nerd Font Ret";
-            size = 11;
-          };
-          extraConfig = {
-            Appearance.UseFontLineChararacters = true;
-            General = {
-              DimWhenInactive = true;
-              TerminalColumns = 80;
-              TerminalRows = 24;
-            };
-            "Interaction Options".AutoCopySelectedText = true;
-            Scrolling.HistorySize = 5000;
-          };
+      }
+      {
+        panels = [
+          {
+            floating = true;
+            lengthMode = "fit";
+            opacity = "translucent";
+            widgets = [
+              # https://github.com/nix-community/plasma-manager/tree/d47428e5390d6a5a8f764808a4db15929347cd77/modules/widgets
+              { kickoff.icon = "nix-snowflake"; } # https://discuss.kde.org/t/need-help-to-find-the-names-locations-of-certain-icons/33988
+              { pager = { }; }
+              {
+                iconTasks = {
+                  iconsOnly = false; # https://github.com/nix-community/plasma-manager/issues/473#issuecomment-2708983491
+                  launchers = [
+                    # https://develop.kde.org/docs/plasma/scripting/keys/
+                    "preferred://filemanager"
+                    "preferred://browser"
+                    "applications:org.kde.konsole.desktop"
+                  ];
+                };
+              }
+              "org.kde.plasma.marginsseparator"
+              { systemTray = { }; }
+              {
+                digitalClock = {
+                  date.format = "isoDate";
+                  time = {
+                    format = "24h";
+                    showSeconds = "always";
+                  };
+                  calendar = {
+                    firstDayOfWeek = "monday";
+                    showWeekNumbers = true;
+                  };
+                };
+              }
+              "org.kde.plasma.showdesktop"
+            ];
+          }
+        ];
+      }
+    ];
+  }
+  {
+    programs.plasma.workspace.splashScreen.theme = "minecraftworldloading-kde-splash";
+    xdg.dataFile."plasma/look-and-feel/minecraftworldloading-kde-splash" = {
+      enable = true;
+      source =
+        (pkgs.fetchFromGitHub {
+          owner = "Samsu-F";
+          repo = "minecraftworldloading-kde-splash";
+          rev = "db3dcf5751afe795d92eec80ff83a16222ca2a18";
+          hash = "sha256-XqzjrHjBDV1xTWQjZ0A4MAu6BlHqlEPNkk/48PjBZEI=";
+        }).outPath;
+    };
+  }
+  {
+    programs.konsole = {
+      enable = true;
+      defaultProfile = "default";
+      profiles.default = {
+        font = {
+          name = "FiraCode Nerd Font Ret";
+          size = 11;
         };
         extraConfig = {
-          KonsoleWindow = {
-            AllowMenuAccelerators = false;
-            RememberWindowSize = false;
-            ShowWindowTitleOnTitleBar = true;
+          Appearance.UseFontLineChararacters = true;
+          General = {
+            DimWhenInactive = true;
+            TerminalColumns = 80;
+            TerminalRows = 24;
           };
-          MainWindow.MenuBar = true;
-          TabBar = {
-            CloseTabOnMiddleMouseButton = true;
-            NewTabButton = true;
-          };
-          SplitView.SplitViewVisibility = "AlwaysHideSplitHeader";
+          "Interaction Options".AutoCopySelectedText = true;
+          Scrolling.HistorySize = 5000;
         };
       };
-    }
-    {
-      programs.librewolf = {
-        profiles.default.extensions.packages = [
-          pkgs.nur.repos.rycee.firefox-addons.plasma-integration
-        ];
-        nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
+      extraConfig = {
+        KonsoleWindow = {
+          AllowMenuAccelerators = false;
+          RememberWindowSize = false;
+          ShowWindowTitleOnTitleBar = true;
+        };
+        MainWindow.MenuBar = true;
+        TabBar = {
+          CloseTabOnMiddleMouseButton = true;
+          NewTabButton = true;
+        };
+        SplitView.SplitViewVisibility = "AlwaysHideSplitHeader";
       };
-    }
-  ];
-}
+    };
+  }
+  {
+    programs.librewolf = {
+      profiles.default.extensions.packages = [
+        pkgs.nur.repos.rycee.firefox-addons.plasma-integration
+      ];
+      nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
+    };
+  }
+]
