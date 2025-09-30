fd39926cc8f901237c9d6c6eec16b16160fd496f
n0099 <n@n0099.net>
2025-09-30 15:06:33 +0000

    * fix `insmod` before each grub entry won't take effect even their files in `/boot/grub/x86_64-efi/*.mod` exists @ boot.nix * wrapping root level `config` with `mkMerge` @ home/kde.nix

--- boot.nix
+++ boot.nix
@@ -8,15 +8,19 @@
       efiSupport = true;
       enableCryptodisk = true;
       device = "nodev";
-      extraPerEntryConfig = ''
+      extraGrubInstallArgs = [
         # https://wiki.archlinux.org/title/GRUB#LUKS2
         # https://github.com/dmoulding/grub-luks2-install/blob/525d757950685888455359618f3fe489c118ca81/grub-luks2-install#L329-L370
-        insmod cryptodisk
-        insmod luks2
-        insmod gcry_rijndael
-        insmod gcry_sha512
-        insmod pbkdf2
-      '';
+        # https://wiki.archlinux.org/title/GRUB#Shim-lock
+        ''
+          --modules=
+            cryptodisk
+            luks2
+            gcry_rijndael
+            gcry_sha512
+            pbkdf2
+        ''
+      ];
     };
     efi.efiSysMountPoint = "/efi";
   };
--- home/kde.nix
+++ home/kde.nix
@@ -1,112 +1,120 @@
 { lib, pkgs, ... }:
 
 {
-  programs.plasma = lib.mkMerge [
-    # run `~/.local/share/plasma-manager/run_all.sh` after `nixos-rebuild switch` to restart plasma-shell and apply settings
+  config = lib.mkMerge [
     {
-      enable = true;
-      overrideConfig = true;
-    }
-    {
-      # https://danbooru.donmai.us/posts?tags=minuo+ratio%3A16%3A9
-      workspace = {
-        wallpaperFillMode = "preserveAspectFit";
-        wallpaper = (
-          (pkgs.fetchurl {
-            url = "https://cdn.donmai.us/original/cb/bf/__doremy_sweet_and_kana_anaberal_touhou_drawn_by_minuo__cbbf70a927d77d9630c1cb00fe4ca300.png";
-            hash = "sha256-V4Lde1mL7Knq1PCqBdTGbQYPdutaDeNeGqHtB7P1b8g=";
-          }).outPath
-        );
-      };
-      kscreenlocker.appearance.wallpaper =
-        (pkgs.fetchurl {
-          url = "https://cdn.donmai.us/original/06/03/__patchouli_knowledge_koakuma_kaku_seiga_hieda_no_akyuu_and_motoori_kosuzu_touhou_drawn_by_minuo__0603e0e274622ad12b048db4470f5bb4.jpg";
-          hash = "sha256-6DqmS10XBjqWFPE+bJD4ZJSWSRQdXNfQDw+tb1zqofo=";
-        }).outPath;
-    }
-    {
-      panels = [
+      programs.plasma = lib.mkMerge [
+        # run `~/.local/share/plasma-manager/run_all.sh` after `nixos-rebuild switch` to restart plasma-shell and apply settings
         {
-          floating = true;
-          lengthMode = "fit";
-          opacity = "translucent";
-          widgets = [
-            # https://github.com/nix-community/plasma-manager/tree/d47428e5390d6a5a8f764808a4db15929347cd77/modules/widgets
-            { kickoff.icon = "nix-snowflake"; } # https://discuss.kde.org/t/need-help-to-find-the-names-locations-of-certain-icons/33988
-            { pager = { }; }
+          enable = true;
+          overrideConfig = true;
+        }
+        {
+          # https://danbooru.donmai.us/posts?tags=minuo+ratio%3A16%3A9
+          workspace = {
+            wallpaperFillMode = "preserveAspectFit";
+            wallpaper = (
+              (pkgs.fetchurl {
+                url = "https://cdn.donmai.us/original/cb/bf/__doremy_sweet_and_kana_anaberal_touhou_drawn_by_minuo__cbbf70a927d77d9630c1cb00fe4ca300.png";
+                hash = "sha256-V4Lde1mL7Knq1PCqBdTGbQYPdutaDeNeGqHtB7P1b8g=";
+              }).outPath
+            );
+          };
+          kscreenlocker.appearance.wallpaper =
+            (pkgs.fetchurl {
+              url = "https://cdn.donmai.us/original/06/03/__patchouli_knowledge_koakuma_kaku_seiga_hieda_no_akyuu_and_motoori_kosuzu_touhou_drawn_by_minuo__0603e0e274622ad12b048db4470f5bb4.jpg";
+              hash = "sha256-6DqmS10XBjqWFPE+bJD4ZJSWSRQdXNfQDw+tb1zqofo=";
+            }).outPath;
+        }
+        {
+          panels = [
             {
-              iconTasks = {
-                iconsOnly = false; # https://github.com/nix-community/plasma-manager/issues/473#issuecomment-2708983491
-                launchers = [
-                  # https://develop.kde.org/docs/plasma/scripting/keys/
-                  "preferred://filemanager"
-                  "preferred://browser"
-                  "applications:org.kde.konsole.desktop"
-                ];
-              };
+              floating = true;
+              lengthMode = "fit";
+              opacity = "translucent";
+              widgets = [
+                # https://github.com/nix-community/plasma-manager/tree/d47428e5390d6a5a8f764808a4db15929347cd77/modules/widgets
+                { kickoff.icon = "nix-snowflake"; } # https://discuss.kde.org/t/need-help-to-find-the-names-locations-of-certain-icons/33988
+                { pager = { }; }
+                {
+                  iconTasks = {
+                    iconsOnly = false; # https://github.com/nix-community/plasma-manager/issues/473#issuecomment-2708983491
+                    launchers = [
+                      # https://develop.kde.org/docs/plasma/scripting/keys/
+                      "preferred://filemanager"
+                      "preferred://browser"
+                      "applications:org.kde.konsole.desktop"
+                    ];
+                  };
+                }
+                "org.kde.plasma.marginsseparator"
+                { systemTray = { }; }
+                {
+                  digitalClock = {
+                    date.format = "isoDate";
+                    time = {
+                      format = "24h";
+                      showSeconds = "always";
+                    };
+                    calendar = {
+                      firstDayOfWeek = "monday";
+                      showWeekNumbers = true;
+                    };
+                  };
+                }
+                "org.kde.plasma.showdesktop"
+              ];
             }
-            "org.kde.plasma.marginsseparator"
-            { systemTray = { }; }
-            {
-              digitalClock = {
-                date.format = "isoDate";
-                time = {
-                  format = "24h";
-                  showSeconds = "always";
-                };
-                calendar = {
-                  firstDayOfWeek = "monday";
-                  showWeekNumbers = true;
-                };
-              };
-            }
-            "org.kde.plasma.showdesktop"
           ];
         }
       ];
     }
+    {
+      programs.konsole = {
+        enable = true;
+        defaultProfile = "default";
+        profiles.default = {
+          font = {
+            name = "FiraCode Nerd Font Ret";
+            size = 11;
+          };
+          extraConfig = {
+            Appearance.UseFontLineChararacters = true;
+            General = {
+              DimWhenInactive = true;
+              TerminalColumns = 80;
+              TerminalRows = 24;
+            };
+            "Interaction Options" = {
+              AutoCopySelectedText = true;
+              TrimLeadingSpacesInSelectedText = true;
+              TrimTrailingSpacesInSelectedText = true;
+            };
+            Scrolling.HistorySize = 5000;
+          };
+        };
+        extraConfig = {
+          KonsoleWindow = {
+            AllowMenuAccelerators = false;
+            RememberWindowSize = false;
+            ShowWindowTitleOnTitleBar = true;
+          };
+          MainWindow.MenuBar = true;
+          TabBar = {
+            CloseTabOnMiddleMouseButton = true;
+            NewTabButton = true;
+          };
+          SplitView.SplitViewVisibility = "AlwaysHideSplitHeader";
+        };
+      };
+    }
+    {
+      programs.librewolf = {
+        profiles.default.extensions.packages = [
+          pkgs.nur.repos.rycee.firefox-addons.plasma-integration
+        ];
+        nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
+      };
+    }
   ];
-  programs.konsole = {
-    enable = true;
-    defaultProfile = "default";
-    profiles.default = {
-      font = {
-        name = "FiraCode Nerd Font Ret";
-        size = 11;
-      };
-      extraConfig = {
-        Appearance.UseFontLineChararacters = true;
-        General = {
-          DimWhenInactive = true;
-          TerminalColumns = 80;
-          TerminalRows = 24;
-        };
-        "Interaction Options" = {
-          AutoCopySelectedText = true;
-          TrimLeadingSpacesInSelectedText = true;
-          TrimTrailingSpacesInSelectedText = true;
-        };
-        Scrolling.HistorySize = 5000;
-      };
-    };
-    extraConfig = {
-      KonsoleWindow = {
-        AllowMenuAccelerators = false;
-        RememberWindowSize = false;
-        ShowWindowTitleOnTitleBar = true;
-      };
-      MainWindow.MenuBar = true;
-      TabBar = {
-        CloseTabOnMiddleMouseButton = true;
-        NewTabButton = true;
-      };
-      SplitView.SplitViewVisibility = "AlwaysHideSplitHeader";
-    };
-  };
-  programs.librewolf = {
-    profiles.default.extensions.packages = [
-      pkgs.nur.repos.rycee.firefox-addons.plasma-integration
-    ];
-    nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];
-  };
 }
