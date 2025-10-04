f987e9025e1b908120e5beaea7c40ae878dfb413
n0099 <n@n0099.net>
2025-10-04 11:10:52 +0000

    * convert function params into options `n0099.containers.*` of nixos module with type checking following https://discourse.nixos.org/t/passing-parameters-into-import/34082/4 - param `configBuilder` as it can be put in `containerConfig.config` and being merged with other functions of `containers.*.config` @ mkContainer.nix

--- mkContainer.nix
+++ mkContainer.nix
@@ -1,12 +1,4 @@
 {
-  name,
-  subnetPrefix,
-  internetAccessInterface ? "",
-  containerConfig ? { },
-}:
-configBuilder:
-{
-  # https://discourse.nixos.org/t/passing-parameters-into-import/34082/2
   config,
   lib,
   pkgs,
@@ -14,42 +6,107 @@ configBuilder:
 }:
 
 {
-  networking.nat = lib.mkIf (internetAccessInterface != "") {
-    # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#give-internet-access
-    enable = true;
-    internalInterfaces = [ ("ve-" + name) ];
-    externalInterface = internetAccessInterface;
+  options = {
+    n0099.containers = lib.mkOption (
+      with {
+        inherit (lib.types)
+          str
+          anything
+          submodule
+          nullOr
+          attrsOf
+          addCheck
+          ;
+        inherit (lib) mkOption;
+      };
+      {
+        type = attrsOf (submodule {
+          options = {
+            subnetPrefix = mkOption {
+              type = addCheck str (
+                value:
+                let
+                  matches = builtins.match "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\." value;
+                  octets = lib.map lib.toIntBase10 matches;
+                  isOctet = octet: octet >= 0 && octet <= 255;
+                in
+                if matches == null then
+                  false
+                # https://datatracker.ietf.org/doc/rfc1918/
+                else if lib.head octets == 10 then
+                  lib.all isOctet (lib.drop 1 octets)
+                else if lib.head octets == 172 then
+                  let
+                    octet = lib.elemAt octets 1;
+                  in
+                  octet >= 16 && octet <= 31 && isOctet (lib.elemAt octets 2)
+                else if
+                  lib.take 2 octets == [
+                    192
+                    168
+                  ]
+                then
+                  isOctet (lib.elemAt octets 2)
+                else
+                  false
+              );
+            };
+            internetAccessInterface = mkOption {
+              type = nullOr str;
+              default = null;
+            };
+            containerConfig = mkOption { type = attrsOf anything; };
+          };
+        });
+      }
+    );
   };
-  containers."${name}" = {
-    autoStart = true;
-    ephemeral = true;
-    privateUsers = "identity"; # https://www.freedesktop.org/software/systemd/man/latest/systemd-nspawn.html#--private-users=
-    privateNetwork = true;
-    hostAddress = subnetPrefix + "1";
-    localAddress = subnetPrefix + "2";
-    config =
-      attrs:
-      lib.mkMerge [
-        (configBuilder attrs)
+  config = {
+    networking.nat = lib.mkMerge (
+      lib.mapAttrsToList
+        (name: container: {
+          # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#give-internet-access
+          enable = true;
+          internalInterfaces = [ ("ve-" + name) ];
+          externalInterface = container.internetAccessInterface;
+        })
+        (lib.filterAttrs (_: container: container.internetAccessInterface != null) config.n0099.containers)
+    );
+    containers = lib.mapAttrs (
+      name: container:
+      (lib.mkMerge [
+        container.containerConfig
         {
-          system = { inherit (config.system) stateVersion; };
-          nixpkgs = lib.mkForce { inherit pkgs; }; # https://github.com/NixOS/nixpkgs/issues/65690
-          networking.firewall = lib.mkIf (containerConfig ? forwardPorts) (
-            let
-              portsByProtocol =
-                protocol:
-                lib.catAttrs "containerPort" (
-                  lib.filter (ports: ports.protocol or "tcp" == protocol) containerConfig.forwardPorts
+          autoStart = true;
+          ephemeral = true;
+          privateUsers = "identity"; # https://www.freedesktop.org/software/systemd/man/latest/systemd-nspawn.html#--private-users=
+          privateNetwork = true;
+          hostAddress = container.subnetPrefix + "1";
+          localAddress = container.subnetPrefix + "2";
+          config =
+            _:
+            lib.mkMerge [
+              {
+                system = { inherit (config.system) stateVersion; };
+                nixpkgs = lib.mkForce { inherit pkgs; }; # https://github.com/NixOS/nixpkgs/issues/65690
+                networking.firewall = lib.mkIf (container.containerConfig ? forwardPorts) (
+                  let
+                    portsByProtocol =
+                      protocol:
+                      lib.catAttrs "containerPort" (
+                        lib.filter (ports: ports.protocol or "tcp" == protocol) container.containerConfig.forwardPorts
+                      );
+                  in
+                  {
+                    # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#real-port-forwarding
+                    allowedTCPPorts = portsByProtocol "tcp";
+                    allowedUDPPorts = portsByProtocol "udp";
+                  }
                 );
-            in
-            {
-              # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#real-port-forwarding
-              allowedTCPPorts = portsByProtocol "tcp";
-              allowedUDPPorts = portsByProtocol "udp";
-            }
-          );
+              }
+            ];
         }
-      ];
-  }
-  // containerConfig;
+      ])
+    ) config.n0099.containers;
+  };
 }
