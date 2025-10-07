fd838561cd1003e081c47360d58be0476e43fcbc
n0099 <n@n0099.net>
2025-10-07 10:58:03 +0000

    * replace `recommendedTlsSettings` with customized configs for TLS 1.3 only clients * enable HTTP/3 and add response header `Alt-Svc`: https://http3-explained.haxx.se/en/h3/h3-altsvc * open default TCP and UDP ports on firewall * move the content of config `virtualHosts.*.extraConfig` that lay in every `server` block from merged options to `appendHttpConfig` that's under `http` block @ nginx.nix
    + `toBeFilled/nginx/dhparam.pem` for `nginx.nix`

--- nginx.nix
+++ nginx.nix
@@ -9,26 +9,69 @@
       attrsOf (submodule {
         config = {
           locations."~ /\.(?!nuxt|cache)".return = 404; # https://github.com/crissyfield/repo-lookout
-          extraConfig = lib.mkBefore ''
-            more_set_headers "Strict-Transport-Security: max-age=63072000; includeSubDomains; preload";
-            more_set_headers "X-Frame-Options: DENY";
-            more_set_headers "X-Content-Type-Options: nosniff";
-            more_set_headers "X-XSS-Protection: 1; mode=block";
-          '';
+          quic = true;
         };
       });
   };
   config = lib.mkMerge [
     {
+      services.nginx = lib.mkMerge [
+        {
+          enable = true;
+          recommendedOptimisation = true;
+          recommendedGzipSettings = true;
+          recommendedBrotliSettings = true;
+          recommendedZstdSettings = true;
+          recommendedProxySettings = true;
+        }
+        {
+          # not using https://github.com/NixOS/nixpkgs/blob/20c4598c84a671783f741e02bf05cbfaf4907cff/nixos/modules/services/web-servers/nginx/default.nix#L207-L221
+          sslProtocols = "TLSv1.3"; # https://caniuse.com/tls1-3
+          sslDhparam = ./toBeFilled/nginx/dhparam.pem;
+          appendHttpConfig = ''
+            ssl_ecdh_curve secp384r1; # https://community.letsencrypt.org/t/would-it-be-a-good-idea-to-use-x448-only-for-maximum-security/239502
+
+            # https://github.com/igrigorik/istlsfastyet.com/issues/63
+            # https://blog.cloudflare.com/optimizing-tls-over-tcp-to-reduce-latency/
+            ssl_buffer_size 1400;
+
+            # https://github.com/mozilla/server-side-tls/issues/135#issuecomment-1293210243
+            # https://upb-syssec.github.io/blog/2023/session-tickets/
+            # https://www.wolfssl.com/tls-session-id-vs-tickets/
+            # https://old.reddit.com/r/nginx/comments/11b1hrm/should_ssl_session_cache_be_turned_off_for_tlsv13/
+            ssl_session_tickets on;
+          '';
+        }
+        {
+          additionalModules = [ pkgs.nginxModules.moreheaders ];
+          appendHttpConfig = ''
+            # https://wiki.nixos.org/wiki/Nginx#Hardened_setup_with_TLS_and_HSTS_preloading
+            map $scheme $hsts_header {
+              https "max-age=63072000; includeSubdomains; preload";
+            }
+            more_set_headers "Strict-Transport-Security $hsts_header";
+            more_set_headers "X-Frame-Options: DENY";
+            more_set_headers "X-Content-Type-Options: nosniff";
+            more_set_headers "X-XSS-Protection: 1; mode=block";
+          '';
+        }
+      ];
+    }
+    {
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    }
+    {
+      networking.firewall.allowedUDPPorts = [ 443 ];
       services.nginx = {
-        enable = true;
-        recommendedOptimisation = true;
-        recommendedTlsSettings = true;
-        recommendedGzipSettings = true;
-        recommendedBrotliSettings = true;
-        recommendedZstdSettings = true;
-        recommendedProxySettings = true;
+        enableQuicBPF = true;
+        package = pkgs.nginxQuic;
         additionalModules = [ pkgs.nginxModules.moreheaders ];
+        appendHttpConfig = ''
+          more_set_headers 'Alt-Svc: h3=":$server_port"; ma=86400'; # https://github.com/NixOS/nixpkgs/pull/449454
+        '';
       };
     }
     (
