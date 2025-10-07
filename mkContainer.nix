7abe2dc5c687a72f3d957f7e37daf8b1d616a271
n0099 <n@n0099.net>
2025-10-07 07:34:22 +0000

    * merge `options.n0099` into parent level @ mkContainer.nix * explicitly set the root-level key `config` @ nginx.nix

--- mkContainer.nix
+++ mkContainer.nix
@@ -6,61 +6,59 @@
 }:
 
 {
-  options = {
-    n0099.containers = lib.mkOption (
-      with {
-        inherit (lib.types)
-          str
-          anything
-          submodule
-          nullOr
-          attrsOf
-          addCheck
-          ;
-        inherit (lib) mkOption;
-      };
-      {
-        type = attrsOf (submodule {
-          options = {
-            subnetPrefix = mkOption {
-              type = addCheck str (
-                value:
+  options.n0099.containers = lib.mkOption (
+    with {
+      inherit (lib.types)
+        str
+        anything
+        submodule
+        nullOr
+        attrsOf
+        addCheck
+        ;
+      inherit (lib) mkOption;
+    };
+    {
+      type = attrsOf (submodule {
+        options = {
+          subnetPrefix = mkOption {
+            type = addCheck str (
+              value:
+              let
+                matches = builtins.match "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\." value;
+                octets = lib.map lib.toIntBase10 matches;
+                isOctet = octet: octet >= 0 && octet <= 255;
+              in
+              if matches == null then
+                false
+              # https://datatracker.ietf.org/doc/rfc1918/
+              else if lib.head octets == 10 then
+                lib.all isOctet (lib.drop 1 octets)
+              else if lib.head octets == 172 then
                 let
-                  matches = builtins.match "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\." value;
-                  octets = lib.map lib.toIntBase10 matches;
-                  isOctet = octet: octet >= 0 && octet <= 255;
+                  octet = lib.elemAt octets 1;
                 in
-                if matches == null then
-                  false
-                # https://datatracker.ietf.org/doc/rfc1918/
-                else if lib.head octets == 10 then
-                  lib.all isOctet (lib.drop 1 octets)
-                else if lib.head octets == 172 then
-                  let
-                    octet = lib.elemAt octets 1;
-                  in
-                  octet >= 16 && octet <= 31 && isOctet (lib.elemAt octets 2)
-                else if
-                  lib.take 2 octets == [
-                    192
-                    168
-                  ]
-                then
-                  isOctet (lib.elemAt octets 2)
-                else
-                  false
-              );
-            };
-            internetAccessInterface = mkOption {
-              type = nullOr str;
-              default = null;
-            };
-            containerConfig = mkOption { type = attrsOf anything; };
+                octet >= 16 && octet <= 31 && isOctet (lib.elemAt octets 2)
+              else if
+                lib.take 2 octets == [
+                  192
+                  168
+                ]
+              then
+                isOctet (lib.elemAt octets 2)
+              else
+                false
+            );
           };
-        });
-      }
-    );
-  };
+          internetAccessInterface = mkOption {
+            type = nullOr str;
+            default = null;
+          };
+          containerConfig = mkOption { type = attrsOf anything; };
+        };
+      });
+    }
+  );
   config = {
     networking.nat = lib.mkMerge (
       lib.mapAttrsToList
--- nginx.nix
+++ nginx.nix
@@ -1,47 +1,49 @@
 { lib, pkgs, ... }:
 
-lib.mkMerge [
-  { services.nginx.enable = true; }
-  (
-    let
-      selfSignedCertDir = "/etc/ssl/self-signed";
-      selfSignedCert = {
-        key = "${selfSignedCertDir}/key.pem";
-        cert = "${selfSignedCertDir}/cert.pem";
-      };
-    in
-    {
-      services.nginx.virtualHosts.default = {
-        serverAliases = [
-          "_"
-          ""
-        ];
-        forceSSL = true;
-        sslCertificate = selfSignedCert.cert;
-        sslCertificateKey = selfSignedCert.key;
-      };
-      systemd.services."nginx-generate-self-signed-cert" = {
-        before = [ "nginx.service" ];
-        wantedBy = [ "nginx.service" ];
-        unitConfig = {
-          ConditionFileNotEmpty = [
-            "!${selfSignedCert.key}"
-            "!${selfSignedCert.cert}"
+{
+  config = lib.mkMerge [
+    {      services.nginx.enable = true;    }
+    (
+      let
+        selfSignedCertDir = "/etc/ssl/self-signed";
+        selfSignedCert = {
+          key = "${selfSignedCertDir}/key.pem";
+          cert = "${selfSignedCertDir}/cert.pem";
+        };
+      in
+      {
+        services.nginx.virtualHosts.default = {
+          serverAliases = [
+            "_"
+            ""
           ];
+          forceSSL = true;
+          sslCertificate = selfSignedCert.cert;
+          sslCertificateKey = selfSignedCert.key;
         };
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${selfSignedCertDir}";
-          # https://stackoverflow.com/questions/10175812/how-can-i-generate-a-self-signed-ssl-certificate-using-openssl/41366949#41366949
-          ExecStart = ''
-            ${pkgs.openssl.bin}/bin/openssl req \
-              -batch -x509 -newkey rsa:4096 -sha256 -days 365 \
-              -keyout ${selfSignedCert.key} \
-              -noenc -out ${selfSignedCert.cert}
-          '';
-          ExecStartPost = "${pkgs.coreutils}/bin/chown -R nginx: ${selfSignedCertDir}";
+        systemd.services."nginx-generate-self-signed-cert" = {
+          before = [ "nginx.service" ];
+          wantedBy = [ "nginx.service" ];
+          unitConfig = {
+            ConditionFileNotEmpty = [
+              "!${selfSignedCert.key}"
+              "!${selfSignedCert.cert}"
+            ];
+          };
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${selfSignedCertDir}";
+            # https://stackoverflow.com/questions/10175812/how-can-i-generate-a-self-signed-ssl-certificate-using-openssl/41366949#41366949
+            ExecStart = ''
+              ${pkgs.openssl.bin}/bin/openssl req \
+                -batch -x509 -newkey rsa:4096 -sha256 -days 365 \
+                -keyout ${selfSignedCert.key} \
+                -noenc -out ${selfSignedCert.cert}
+            '';
+            ExecStartPost = "${pkgs.coreutils}/bin/chown -R nginx: ${selfSignedCertDir}";
+          };
         };
-      };
-    }
-  )
-]
+      }
+    )
+  ];
+}
