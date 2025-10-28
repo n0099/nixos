5f625d7a482120a4501c28e9505717f596b07cb2
n0099 <n@n0099.net>
2025-10-28 02:00:00 +0000

    * split into multiple nixos modules @ extendContainer.nix

--- extendContainer.nix
+++ extendContainer.nix
@@ -5,39 +5,52 @@
   ...
 }:
 
+with {
+  inherit (lib) mkOption;
+  inherit (lib.types) attrsOf submodule;
+};
 {
-  options.containers = lib.mkOption {
-    type =
-      with {
-        inherit (lib.types)
-          str
-          port
-          nullOr
-          listOf
-          attrsOf
-          submodule
-          addCheck
-          ;
+  imports = [
+    {
+      options.containers = mkOption {
+        type = attrsOf (submodule {
+          config = {
+            autoStart = true;
+            ephemeral = true;
+            privateUsers = "identity"; # https://www.freedesktop.org/software/systemd/man/latest/systemd-nspawn.html#--private-users=
+            privateNetwork = true;
+            config = {
+              system = { inherit (config.system) stateVersion; };
+              nixpkgs = lib.mkForce { inherit pkgs; }; # https://github.com/NixOS/nixpkgs/issues/65690
+            };
+          };
+        });
       };
-      attrsOf (
-        submodule (
-          { ... }@container:
+    }
+    {
+      options.containers =
+        with {
+          inherit (lib)
+            map
+            all
+            head
+            drop
+            take
+            elemAt
+            toIntBase10
+            ;
+          inherit (lib.types)
+            addCheck
+            str
+            ;
+        };
+        mkOption {
+          type = attrsOf (
+            submodule (
+              { ... }@container:
 
-          {
-            options.n0099 =
-              with {
-                inherit (lib)
-                  mkOption
-                  map
-                  all
-                  head
-                  drop
-                  take
-                  elemAt
-                  toIntBase10
-                  ;
-              }; {
-                subnetPrefix = mkOption {
+              {
+                options.n0099.subnetPrefix = mkOption {
                   type = addCheck str (
                     value:
                     let
@@ -66,90 +79,108 @@
                       false
                   );
                 };
-                internetAccessInterface = mkOption {
-                  type = nullOr str;
-                  default = null;
+                config = {
+                  hostAddress = "${container.config.n0099.subnetPrefix}1";
+                  localAddress = "${container.config.n0099.subnetPrefix}2";
                 };
-                forwardPorts = mkOption {
-                  type = listOf attrsOf {
+              }
+            )
+          );
+        };
+    }
+    {
+      options.containers = mkOption {
+        type = attrsOf (submodule {
+          options.n0099.internetAccessInterface = mkOption {
+            type = with lib.types; nullOr str;
+            default = null;
+          };
+        });
+      };
+      config.networking.nat = lib.mkMerge (
+        lib.mapAttrsToList
+          (name: container: {
+            # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#give-internet-access
+            # https://wiki.archlinux.org/title/Systemd-nspawn#Use_NAT_networking
+            enable = true;
+            internalInterfaces = [ "ve-${name}" ];
+            externalInterface = container.n0099.internetAccessInterface;
+          })
+          (lib.filterAttrs (_: container: container.n0099.internetAccessInterface != null) config.containers)
+      );
+    }
+    {
+      options.containers = mkOption {
+        type = attrsOf (
+          submodule (
+            { ... }@container:
+
+            {
+              options.n0099.forwardPorts = mkOption {
+                type =
+                  with {
+                    inherit (lib.types)
+                      str
+                      port
+                      either
+                      listOf
+                      ;
+                  };
+                  listOf attrsOf {
                     hostListenStreams = listOf str; # https://github.com/NixOS/nixpkgs/blob/78e34d1667d32d8a0ffc3eba4591ff256e80576e/nixos/lib/systemd-unit-options.nix#L628
                     containerPort = port;
                     protocol = either "tcp" "udp";
                   };
+              };
+              config.config.networking.firewall = lib.mkIf (container.config.n0099 ? forwardPorts) (
+                let
+                  portsByProtocol =
+                    protocol:
+                    lib.catAttrs "containerPort" (
+                      lib.filter (ports: ports.protocol or "tcp" == protocol) container.config.n0099.forwardPorts
+                    );
+                in
+                {
+                  # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#real-port-forwarding
+                  allowedTCPPorts = portsByProtocol "tcp";
+                  allowedUDPPorts = portsByProtocol "udp";
+                }
+              );
+            }
+          )
+        );
+      };
+      config.systemd = lib.mkMerge lib.mapAttrsToList (
+        name: container:
+        lib.map (
+          forwardPort:
+          let
+            unitName = "container@${name}-forward-port:${forwardPort.containerPort}.socket";
+          in
+          {
+            # https://www.freedesktop.org/software/systemd/man/latest/systemd-socket-proxyd.html#Simple%20Example
+            sockets.${unitName} = {
+              listenStreams = forwardPort.hostListenStreams;
+              wantedBy = [ "sockets.target" ];
+            };
+            services.${unitName} =
+              let
+                requiredUnits = [
+                  "container@${name}.service"
+                  "${unitName}.socket"
+                ];
+              in
+              {
+                requires = requiredUnits;
+                after = requiredUnits;
+                serviceConfig = {
+                  Type = "notify";
+                  ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd ${container.config.localAddress}:${forwardPort.containerPort}"; # not using `systemd.services.*.script` to prevent https://github.com/systemd/systemd/blob/a6146b250efcce88ed836e62ead527f699049a8e/src/socket-proxy/socket-proxyd.c#L674-L678
                 };
               };
-            config = {
-              autoStart = true;
-              ephemeral = true;
-              privateUsers = "identity"; # https://www.freedesktop.org/software/systemd/man/latest/systemd-nspawn.html#--private-users=
-              privateNetwork = true;
-              hostAddress = "${container.config.n0099.subnetPrefix}1";
-              localAddress = "${container.config.n0099.subnetPrefix}2";
-              config = {
-                system = { inherit (config.system) stateVersion; };
-                nixpkgs = lib.mkForce { inherit pkgs; }; # https://github.com/NixOS/nixpkgs/issues/65690
-                networking.firewall = lib.mkIf (container.config.n0099 ? forwardPorts) (
-                  let
-                    portsByProtocol =
-                      protocol:
-                      lib.catAttrs "containerPort" (
-                        lib.filter (ports: ports.protocol or "tcp" == protocol) container.config.n0099.forwardPorts
-                      );
-                  in
-                  {
-                    # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#real-port-forwarding
-                    allowedTCPPorts = portsByProtocol "tcp";
-                    allowedUDPPorts = portsByProtocol "udp";
-                  }
-                );
-              };
-            };
           }
-        )
-      );
-  };
-  config = {
-    systemd = lib.mkMerge lib.mapAttrsToList (
-      name: container:
-      lib.map (
-        forwardPort:
-        let
-          unitName = "container@${name}-forward-port:${forwardPort.containerPort}.socket";
-        in
-        {
-          # https://www.freedesktop.org/software/systemd/man/latest/systemd-socket-proxyd.html#Simple%20Example
-          sockets.${unitName} = {
-            listenStreams = forwardPort.hostListenStreams;
-            wantedBy = [ "sockets.target" ];
-          };
-          services.${unitName} =
-            let
-              requiredUnits = [
-                "container@${name}.service"
-                "${unitName}.socket"
-              ];
-            in
-            {
-              requires = requiredUnits;
-              after = requiredUnits;
-              serviceConfig = {
-                Type = "notify";
-                ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd ${container.config.localAddress}:${forwardPort.containerPort}"; # not using `systemd.services.*.script` to prevent https://github.com/systemd/systemd/blob/a6146b250efcce88ed836e62ead527f699049a8e/src/socket-proxy/socket-proxyd.c#L674-L678
-              };
-            };
-        }
-      ) container.forwardPorts
-    ) config.containers;
-    networking.nat = lib.mkMerge (
-      lib.mapAttrsToList
-        (name: container: {
-          # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html#give-internet-access
-          # https://wiki.archlinux.org/title/Systemd-nspawn#Use_NAT_networking
-          enable = true;
-          internalInterfaces = [ "ve-${name}" ];
-          externalInterface = container.n0099.internetAccessInterface;
-        })
-        (lib.filterAttrs (_: container: container.n0099.internetAccessInterface != null) config.containers)
-    );
-  };
+        ) container.forwardPorts
+      ) config.containers;
+    }
+  ];
 }
