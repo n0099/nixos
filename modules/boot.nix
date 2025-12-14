ea92e1ca910ddc0ce139cce8f1a1caa0ec5d7a5b
n0099 <n@n0099.net>
2025-12-14 09:30:09 +0000

    * move parts about agenix in `flake.nix` before commit f12b6542491d3729019f75b341bf566a5b8d96f5 into here @ secrets.nix * using `lib` from `flake-parts` instead of nixos modules to reduce nesting level @ boot.nix @ modules

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,28 +1,27 @@
-{
-  flake.modules.nixos.boot =
-    { lib, ... }:
+{ lib, ... }:
 
-    {
-      boot.loader = {
-        systemd-boot.enable = false |> lib.mkForce;
-        efi.efiSysMountPoint = "/efi";
-        grub = {
-          enable = true;
-          device = "nodev";
-          efiSupport = true;
-          enableCryptodisk = true;
-          extraGrubInstallArgs = [
-            # https://wiki.archlinux.org/title/GRUB#LUKS2
-            # https://github.com/dmoulding/grub-luks2-install/blob/525d757950685888455359618f3fe489c118ca81/grub-luks2-install#L329-L370
-            # https://wiki.archlinux.org/title/GRUB#Shim-lock
-            ''
-              --modules=
-                luks2
-                gcry_sha512
-                pbkdf2
-            ''
-          ];
-        };
+{
+  flake.modules.nixos.boot = {
+    boot.loader = {
+      systemd-boot.enable = false |> lib.mkForce;
+      efi.efiSysMountPoint = "/efi";
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        enableCryptodisk = true;
+        extraGrubInstallArgs = [
+          # https://wiki.archlinux.org/title/GRUB#LUKS2
+          # https://github.com/dmoulding/grub-luks2-install/blob/525d757950685888455359618f3fe489c118ca81/grub-luks2-install#L329-L370
+          # https://wiki.archlinux.org/title/GRUB#Shim-lock
+          ''
+            --modules=
+              luks2
+              gcry_sha512
+              pbkdf2
+          ''
+        ];
       };
     };
+  };
 }
--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,10 +1,13 @@
-{
-  flake.modules.nixos.secrets =
-    { lib, ... }:
+{ lib, inputs, ... }:
 
-    {
-      age.secrets = # https://github.com/ryantm/agenix/pull/348
-        (name: { file = ./${name}.age; })
-        |> lib.genAttrs (import ../secrets/secrets.nix |> lib.attrNames |> map (lib.removeSuffix ".age"));
+{
+  flake.modules.nixos = {
+    agenix = with inputs.agenix; {
+      imports = [ nixosModules.default ];
+      config.environment.systemPackages = [ packages."x86_64-linux".default ];
     };
+    secrets.config.age.secrets = # https://github.com/ryantm/agenix/pull/348
+      (name: { file = ./${name}.age; })
+      |> lib.genAttrs (import ../secrets/secrets.nix |> lib.attrNames |> map (lib.removeSuffix ".age"));
+  };
 }
