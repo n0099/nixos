f796a3eeb824442e9e0305a528c5d523327ba8cc
n0099 <n@n0099.net>
2025-12-14 15:13:55 +0000

    * replace listing all nixos and home-manager modules being imported in `flake.nixosConfigurations` with `import-tree` and `base/modules/nixosConfig.nix` @ flake.nix $ git submodule update --remote

--- flake.nix
+++ flake.nix
@@ -18,57 +18,14 @@
   outputs =
     { base, ... }@inputs:
 
-    base.inputs.flake-parts.outputs.lib.mkFlake { inherit inputs; } (
-      { lib, ... }:
-
-      (
-        let
-          specialArgs = {
-            # https://jade.fyi/blog/flakes-arent-real/#injecting-dependencies
-            inputs = lib.removeAttrs inputs [ "base" ] // inputs.base.inputs;
-          };
-        in
-        {
-          systems = [ "x86_64-linux" ];
-          # systems = base.outputs.systems;
-          flake.nixosConfigurations.nixos = base.inputs.nixpkgs.lib.nixosSystem {
-            # system is not needed with freshly generated hardware-configuration.nix
-            system = "x86_64-linux"; # or set nixpkgs.hostPlatform in a module.
-            inherit specialArgs;
-            modules = [
-              {
-                imports = lib.attrValues base.outputs.nixosModules ++ [
-                  inputs.nur.modules.nixos.default # https://nur.nix-community.org/documentation/#using-the-flake-in-nixos
-                  ./configuration.nix
-                  ./boot.nix
-                  ./zfs.nix
-                  ./march.nix
-                  ./stdenv.nix
-                  ./desktop.nix
-                  ./malloc.nix
-                  ./kde.nix
-                  ./font.nix
-                  ./game.nix
-                  ./substituters.nix
-                  ./containers
-                  ./secrets
-                  ./proxy.nix
-                  ./wireguard.nix
-                ];
-              }
-              {
-                home-manager = {
-                  extraSpecialArgs = specialArgs; # https://discourse.nixos.org/t/pass-specialargs-to-the-home-manager-module/33068
-                  users.n0099.imports = lib.attrValues base.outputs.homeModules ++ [
-                    inputs.plasma-manager.homeModules.plasma-manager
-                    inputs.nur.modules.homeManager.default # https://haseebmajid.dev/posts/2023-06-22-til-use-nur-with-home-manager-flake/
-                    ./home/n0099.nix
-                  ];
-                };
-              }
-            ];
-          };
-        }
-      )
-    );
+    with { inherit (base.inputs) flake-parts import-tree; };
+    flake-parts.lib.mkFlake { inputs = base.inputs // inputs; } {
+      imports = [
+        flake-parts.flakeModules.modules
+        (import-tree [
+          ./base/modules
+          ./modules
+        ])
+      ];
+    };
 }
