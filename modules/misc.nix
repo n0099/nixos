330948d64e4be10bb7c539a884d2a2160b0ede8f
n0099 <n@n0099.net>
2025-12-23 12:41:00 +0000

    * move top-level configs for `nix` from `nix.nix` to `misc.nix` $ mv {system,misc}.nix $ mv {extendC,c}ontainer.nix @ modules

--- modules/misc.nix
+++ modules/misc.nix
@@ -8,12 +8,6 @@
     }:
 
     {
-      nix = {
-        channel.enable = false; # https://github.com/NixOS/nix/issues/2982#issuecomment-2477618346
-        # https://wiki.nixos.org/wiki/Storage_optimization
-        gc.automatic = true;
-        optimise.automatic = true;
-      };
       time.timeZone = "UTC";
       i18n.defaultLocale = "C.UTF-8";
       users = {
--- modules/nix.nix
+++ modules/nix.nix
@@ -13,5 +13,11 @@ in
 {
   imports = [ inputs.flake-file.flakeModules.default ];
   flake-file.nixConfig.experimental-features = features;
-  flake.modules.nixos.nix.nix.settings.experimental-features = features;
+  flake.modules.nixos.nix.nix = {
+    settings.experimental-features = features;
+    channel.enable = false; # https://github.com/NixOS/nix/issues/2982#issuecomment-2477618346
+    # https://wiki.nixos.org/wiki/Storage_optimization
+    gc.automatic = true;
+    optimise.automatic = true;
+  };
 }
