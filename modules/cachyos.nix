98400b43ba1d7b1f3f7eb9ac5afe75b23e45f1fb
n0099 <git@n0099.com>
2026-01-12 01:45:59 +0000

    * only enable kconfig `LTO_CLANG_FULL` when the variant is `-lto` * set the default value of option `n0099.cachyos.baseKernel` to `pkgs.linux_latest` to match with upstream * fix `Error: modDirVersion 6.18.4-cachyos-lto specified in the Nix expression is wrong, it should be: 6.18.4` due to https://github.com/xddxdd/nix-cachyos-kernel/blob/8f448feca5af93c79fdd2187af392f692192b2f3/kernel-cachyos/mkCachyKernel.nix#L156 @ modules/cachyos.nix

--- modules/cachyos.nix
+++ modules/cachyos.nix
@@ -20,18 +20,24 @@
       options.n0099.cachyos = {
         enable = lib.mkEnableOption "";
         variant = lib.mkOption { type = lib.types.str; }; # https://wiki.cachyos.org/features/kernel/#variants
-        baseKernel = lib.mkOption { type = lib.types.package; };
+        baseKernel = lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.linux_latest; # https://github.com/xddxdd/nix-cachyos-kernel/blob/dc3941ceb1cc0b303ddefc5e5fa1577a2d7856d7/kernel-cachyos/default.nix#L16
+        };
       };
       config = {
         nixpkgs.overlays = [ inputs.nix-cachyos-kernel.outputs.overlay ]; # https://github.com/xddxdd/nix-cachyos-kernel/blob/dc3941ceb1cc0b303ddefc5e5fa1577a2d7856d7/flake.nix#L95
         boot.kernelPackages = pkgs.cachyosKernels."linuxPackages-cachyos-${cfg.variant}".extend (
           final: prev: {
             kernel = prev.kernel.override {
-              inherit (cfg.baseKernel) version src; # https://github.com/xddxdd/nix-cachyos-kernel/blob/dc3941ceb1cc0b303ddefc5e5fa1577a2d7856d7/kernel-cachyos/default.nix#L16
-              structuredExtraConfig = with lib.kernel; {
-                LTO_CLANG_FULL = yes;
-                LTO_CLANG_THIN = no; # https://github.com/xddxdd/nix-cachyos-kernel/blob/52d03c7f4e6f78cbf9e1ec1d78101b2369ff8f7c/kernel-cachyos/mkCachyKernel.nix#L108-L111
-              };
+              inherit (cfg.baseKernel) version src;
+              modDirVersion = final.kernel.version; # https://github.com/NixOS/nixpkgs/blob/d03088749a110d52a4739348f39a63f84bb0be14/pkgs/os-specific/linux/kernel/manual-config.nix#L326
+              structuredExtraConfig =
+                with lib.kernel;
+                lib.mkIf (lib.hasSuffix "-lto" cfg.variant) {
+                  LTO_CLANG_FULL = yes; # https://www.kernelconfig.io/config_lto_clang_full
+                  LTO_CLANG_THIN = no; # https://github.com/xddxdd/nix-cachyos-kernel/blob/52d03c7f4e6f78cbf9e1ec1d78101b2369ff8f7c/kernel-cachyos/mkCachyKernel.nix#L108-L111
+                };
             };
           }
         );
