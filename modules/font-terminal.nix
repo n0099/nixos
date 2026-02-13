4a6059a1f6a68fed7da5f061451706257bca4f61
n0099 <git@n0099.com>
2026-02-13 14:34:19 +0000

    * set font `FiraCode Nerd Font` as the alias of `monospace`: https://wiki.archlinux.org/title/Font_configuration#Alias @ modules/font-terminal.nix

--- modules/font-terminal.nix
+++ modules/font-terminal.nix
@@ -1,13 +1,19 @@
+let
+  fontFamily = "FiraCode Nerd Font Ret";
+in
 {
   flake.modules = {
     nixos.font-terminal =
       { pkgs, ... }:
 
       {
-        fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
+        fonts = {
+          packages = with pkgs; [ nerd-fonts.fira-code ];
+          fontconfig.defaultFonts.monospace = [ fontFamily ];
+        };
       };
     homeManager.font-terminal.programs.konsole.profiles.default.font = {
-      name = "FiraCode Nerd Font Ret";
+      name = fontFamily; # Konsole trying to use the `Hack` font by default: https://lists.fedorahosted.org/archives/list/kde@lists.fedoraproject.org/thread/NUW3L6VODVB3EERMQHCMRSLAKUYUW2S5/
       size = 11;
     };
   };
