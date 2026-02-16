d2ed15b32333319148c4897f7949a493b9e60341
n0099 <git@n0099.com>
2026-02-16 18:57:06 +0000

    * set the valut of container config `n0099.outboundInterface` with `toBeFilled/vaultwarden/outboundInterface` @ modules/vaultwarden.nix

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -19,35 +19,47 @@
         ];
         virtualHosts.${domain}.locations."/".proxyWebsockets = true; # https://github.com/dani-garcia/vaultwarden/wiki/Enabling-WebSocket-notifications
       };
-      containers.vaultwarden = {
-        n0099.subnetPrefix = "172.16.0.";
-        bindMounts = {
-          "/var/lib/vaultwarden" = {
-            hostPath = "/srv/vaultwarden";
-            isReadOnly = false;
+      containers.vaultwarden = lib.mkMerge [
+        {
+          n0099.subnetPrefix = "172.16.0.";
+          bindMounts = {
+            "/var/lib/vaultwarden" = {
+              hostPath = "/srv/vaultwarden";
+              isReadOnly = false;
+            };
           };
-        };
-        config = {
-          networking.firewall.allowedTCPPorts = [ listen.port ];
-          services.vaultwarden = {
-            enable = true;
-            config = lib.mkMerge [
-              {
+          config = {
+            networking.firewall.allowedTCPPorts = [ listen.port ];
+            services.vaultwarden = {
+              enable = true;
+              config = {
                 ROCKET_ADDRESS = listen.address;
                 DOMAIN = "https://${domain}";
                 SIGNUPS_ALLOWED = false;
                 INVITATIONS_ALLOWED = false;
-              }
-              (lib.mkIf (lib.pathExists ../toBeFilled/vaultwarden/pushInstallation/id) {
+              };
+            };
+          };
+        }
+        {
+          # website favicon
+          # https://github.com/dani-garcia/vaultwarden/wiki/Using-custom-website-icons#website-icon-troubleshooting
+          # https://old.reddit.com/r/vaultwarden/comments/15jrn01/is_it_possible_to_have_icon_without_internet/
+          # and push notification based sync
+          # https://github.com/dani-garcia/vaultwarden/wiki/Enabling-Mobile-Client-push-notification
+          # https://github.com/dani-garcia/vaultwarden/blob/36f0620fd19af0816a7ee2ed882d368b1b298ddc/src/api/push.rs#L68
+          # require internet access
+          n0099.outboundInterface = readToBeFilled "outboundInterface";
+          config.services.vaultwarden.config =
+            lib.mkIf (lib.pathExists ../toBeFilled/vaultwarden/pushInstallation/id)
+              {
                 PUSH_ENABLED = true;
                 PUSH_INSTALLATION_ID = readToBeFilled "pushInstallation/id";
                 PUSH_INSTALLATION_KEY = readToBeFilled "pushInstallation/key";
                 PUSH_RELAY_URI = readToBeFilled "pushRelayUri";
                 PUSH_IDENTITY_URI = readToBeFilled "pushIdentityUri";
-              })
-            ];
-          };
-        };
-      };
+              };
+        }
+      ];
     };
 }
