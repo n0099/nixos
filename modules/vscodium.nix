1b2e538aefb11960e3deb3d06bbecfcc6e0f35e3
n0099 <git@n0099.com>
2026-02-17 20:28:52 +0000

    + add variant that disabled config `diffEditor.renderSideBySide` for each profile @ modules/vscodium.nix

--- modules/vscodium.nix
+++ modules/vscodium.nix
@@ -52,102 +52,118 @@
         programs.vscode = {
           enable = true;
           package = pkgs.vscodium;
-          profiles = {
-            default = lib.mkMerge [
-              {
-                userSettings =
-                  flatSettings {
-                    editor = {
-                      fontLigatures = true;
-                      bracketPairColorization.enabled = true;
+          profiles =
+            {
+              default = lib.mkMerge [
+                {
+                  userSettings =
+                    flatSettings {
+                      editor = {
+                        fontLigatures = true;
+                        bracketPairColorization.enabled = true;
+                      };
+                      diffEditor = {
+                        ignoreTrimWhitespace = false;
+                        experimental.showMoves = true;
+                        hideUnchangedRegions.enabled = true;
+                      };
+                      files.autoGuessEncoding = true;
+                    }
+                    // {
+                      "files.associations"."*.json5" = "jsonc";
                     };
-                    diffEditor = {
-                      ignoreTrimWhitespace = false;
-                      experimental.showMoves = true;
-                      hideUnchangedRegions.enabled = true;
+                  extensions = with extensions; [
+                    zxh404.vscode-proto3
+                    ahmadalli.vscode-nginx-conf
+                    ms-vscode.hexeditor
+                    mechatroner.rainbow-csv
+
+                    editorconfig.editorconfig
+                    mikestead.dotenv
+                    (
+                      mkhl.direnv # https://github.com/NixOS/nixpkgs/pull/491239
+                      |> overrideExtensionExecutableConfig "direnv.path.executable" pkgs.direnv
+                    )
+                    jnoortheen.nix-ide
+
+                    qcz.text-power-tools
+
+                    davidanson.vscode-markdownlint
+                    shd101wyy.markdown-preview-enhanced
+                    pkgs.vscode-marketplace-release.bierner.markdown-preview-github-styles # https://github.com/mjbvz/vscode-github-markdown-preview-style/issues/59#issuecomment-1499414723
+
+                    (
+                      mads-hartmann.bash-ide-vscode # https://github.com/NixOS/nixpkgs/pull/491237
+                      |> overrideExtensionExecutableConfig "bashIde.shellcheckPath" pkgs.shellcheck
+                      |> overrideExtensionExecutableConfig "bashIde.shfmt.path" pkgs.shfmt
+                    )
+
+                    github.vscode-github-actions
+                  ];
+                }
+                {
+                  extensions = [ extensions.redhat.vscode-yaml ];
+                  userSettings."redhat.telemetry.enabled" = false;
+                }
+                {
+                  extensions = with extensions; [
+                    docker.docker
+                    # ms-azuretools.vscode-containers
+                  ];
+                  userSettings."docker.lsp.telemetry" = "off";
+                }
+                {
+                  extensions = [ extensions.eamodio.gitlens ];
+                  userSettings = flatSettings {
+                    gitlens = {
+                      ai.enabled = false;
+                      telemetry.enabled = false;
+                      advanced.skipOnboarding = true;
+                      plusFeatures.enabled = false;
+                      launchpad.indicator.enabled = false;
                     };
-                    files.autoGuessEncoding = true;
-                  }
-                  // {
-                    "files.associations"."*.json5" = "jsonc";
                   };
-                extensions = with extensions; [
-                  zxh404.vscode-proto3
-                  ahmadalli.vscode-nginx-conf
-                  ms-vscode.hexeditor
-                  mechatroner.rainbow-csv
-
-                  editorconfig.editorconfig
-                  mikestead.dotenv
-                  (
-                    mkhl.direnv # https://github.com/NixOS/nixpkgs/pull/491239
-                    |> overrideExtensionExecutableConfig "direnv.path.executable" pkgs.direnv
-                  )
-                  jnoortheen.nix-ide
-
-                  qcz.text-power-tools
-
-                  davidanson.vscode-markdownlint
-                  shd101wyy.markdown-preview-enhanced
-                  pkgs.vscode-marketplace-release.bierner.markdown-preview-github-styles # https://github.com/mjbvz/vscode-github-markdown-preview-style/issues/59#issuecomment-1499414723
-
-                  (
-                    mads-hartmann.bash-ide-vscode # https://github.com/NixOS/nixpkgs/pull/491237
-                    |> overrideExtensionExecutableConfig "bashIde.shellcheckPath" pkgs.shellcheck
-                    |> overrideExtensionExecutableConfig "bashIde.shfmt.path" pkgs.shfmt
-                  )
-
-                  github.vscode-github-actions
-                ];
-              }
-              {
-                extensions = [ extensions.redhat.vscode-yaml ];
-                userSettings."redhat.telemetry.enabled" = false;
-              }
-              {
-                extensions = with extensions; [
-                  docker.docker
-                  # ms-azuretools.vscode-containers
-                ];
-                userSettings."docker.lsp.telemetry" = "off";
-              }
-              {
-                extensions = [ extensions.eamodio.gitlens ];
-                userSettings = flatSettings {
-                  gitlens = {
-                    ai.enabled = false;
-                    telemetry.enabled = false;
-                    advanced.skipOnboarding = true;
-                    plusFeatures.enabled = false;
-                    launchpad.indicator.enabled = false;
-                  };
+                }
+                {
+                  extensions = [ extensions.atommaterial.a-file-icon-vscode ];
+                  userSettings."workbench.iconTheme" = "a-file-icon-vscode";
+                }
+              ];
+              Frontend =
+                let
+                  default = config.programs.vscode.profiles.default;
+                in
+                {
+                  userSettings = default.userSettings;
+                  extensions =
+                    with extensions;
+                    default.extensions
+                    ++ [
+                      vue.volar
+                      dbaeumer.vscode-eslint
+                      stylelint.vscode-stylelint
+                      webben.browserslist
+                      kimuson.ts-type-expand
+                      antfu.goto-alias
+                      arcanis.vscode-zipfs
+                    ];
                 };
-              }
-              {
-                extensions = [ extensions.atommaterial.a-file-icon-vscode ];
-                userSettings."workbench.iconTheme" = "a-file-icon-vscode";
-              }
-            ];
-            Frontend =
+            }
+            |> lib.concatMapAttrs (
+              name: profile:
               let
-                default = config.programs.vscode.profiles.default;
+                name' = if name == "default" then "Default" else name; # https://github.com/nix-community/home-manager/blob/0d782ee42c86b196acff08acfbf41bb7d13eed5b/modules/programs/vscode/default.nix#L85
               in
               {
-                userSettings = default.userSettings;
-                extensions =
-                  with extensions;
-                  default.extensions
-                  ++ [
-                    vue.volar
-                    dbaeumer.vscode-eslint
-                    stylelint.vscode-stylelint
-                    webben.browserslist
-                    kimuson.ts-type-expand
-                    antfu.goto-alias
-                    arcanis.vscode-zipfs
-                  ];
-              };
-          };
+                ${name} = profile;
+                "${name'} Inline Diff" = lib.mkMerge [
+                  profile
+                  {
+                    userSettings."diffEditor.renderSideBySide" = false;
+                  }
+                ];
+              }
+            );
         };
       };
   };
