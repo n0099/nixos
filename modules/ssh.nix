3308bd9485450348a22f3e3099fbed75925f8fb1
n0099 <git@n0099.com>
2026-03-06 02:44:18 +0000

    + add public key of `ssh://github.com` to global known hosts to prevent TOFU MITM @ modules/ssh.nix

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,15 +1,21 @@
 {
   flake.modules = {
-    nixos.ssh.services.openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        PermitRootLogin = "no";
-      }
-      // {
-        ClientAliveInterval = 15;
+    nixos.ssh = {
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+        }
+        // {
+          PermitRootLogin = "no";
+          ClientAliveInterval = 15;
+        };
       };
+      # https://github.com/nix-community/srvos/blob/3fb70232a9fe901d943d0340ab99fa36abb62418/shared/common/well-known-hosts.nix#L7
+      # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+      programs.ssh.knownHosts."github.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
     homeManager.ssh = {
       services.ssh-agent.enable = true;
