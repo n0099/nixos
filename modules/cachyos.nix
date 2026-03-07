0d5ac7248cb8a7ec38115d15e7128d9c3de0734e
n0099 <git@n0099.com>
2026-03-07 02:38:57 +0000

    * enable AutoFDO by default @ modules/cachyos.nix

--- modules/cachyos.nix
+++ modules/cachyos.nix
@@ -22,15 +22,16 @@
         variant = lib.mkOption { type = lib.types.str; }; # https://wiki.cachyos.org/features/kernel/#variants
         baseKernel = lib.mkOption {
           type = lib.types.package;
-          default = pkgs.linux_latest; # https://github.com/xddxdd/nix-cachyos-kernel/blob/dc3941ceb1cc0b303ddefc5e5fa1577a2d7856d7/kernel-cachyos/default.nix#L16
+          default = pkgs.linux_latest;
         };
       };
       config = lib.mkIf cfg.enable {
         boot.kernelPackages =
           {
+            autofdo = true; # https://cachyos.org/blog/2411-kernel-autofdo/
             lto = "full"; # https://www.kernelconfig.io/config_lto_clang_full
             stdenv = pkgs.clangStdenv; # using our stdenv with possibly configured `hostPlatform.gcc.march`
-            inherit (cfg.baseKernel) version src;
+            inherit (cfg.baseKernel) version src; # https://github.com/xddxdd/nix-cachyos-kernel/blob/dc3941ceb1cc0b303ddefc5e5fa1577a2d7856d7/kernel-cachyos/default.nix#L16
           }
           |> inputs.nix-cachyos-kernel.outputs.packages.${pkgs.stdenv.system}."linux-cachyos-${cfg.variant}".override # https://github.com/xddxdd/nix-cachyos-kernel/issues/23#issuecomment-3764296449
           |> pkgs.linuxKernel.packagesFor;
