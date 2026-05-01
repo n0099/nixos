a92295ec76a5084a229488e927ef558de08bb9e0
n0099 <git@n0099.com>
2026-05-01 19:31:22 +0000

    + add package https://github.com/libratbag/piper and enable its daemon to set mouse DPI to its firmwire @ modules/osu.nix

--- modules/osu.nix
+++ modules/osu.nix
@@ -1,11 +1,26 @@
-{
-  flake.modules = {
-    nixos.osu.n0099.permittedUnfreePackages = [ "osu-lazer-bin" ];
-    homeManager.osu =
-      { pkgs, ... }:
+{ lib, ... }:
 
-      {
-        home.packages = [ pkgs.osu-lazer-bin ];
-      };
-  };
-}
+lib.foldl lib.recursiveUpdate { } [
+  {
+    flake.modules = {
+      nixos.osu.n0099.permittedUnfreePackages = [ "osu-lazer-bin" ];
+      homeManager.osu =
+        { pkgs, ... }:
+
+        {
+          home.packages = [ pkgs.osu-lazer-bin ];
+        };
+    };
+  }
+  {
+    flake.modules = {
+      nixos.ratbag.services.ratbagd.enable = true;
+      homeManager.piper =
+        { pkgs, ... }:
+
+        {
+          home.packages = [ pkgs.piper ];
+        };
+    };
+  }
+]
