2a22d633d8c4859bad2b4ffc67fd617e93ef623c
n0099 <git@n0099.com>
2026-06-06 01:03:51 +0000

    - remove changes from https://github.com/NixOS/nixpkgs/pull/457493 as it's appeared in 26.05 @ modules/march.nix

--- modules/march.nix
+++ modules/march.nix
@@ -1,6 +1,6 @@
 {
   flake.modules.nixos.march =
-    { pkgs, lib, ... }:
+    { lib, ... }:
 
     lib.mkMerge [
       {
@@ -12,40 +12,6 @@
       {
         # https://stackoverflow.com/questions/69971612/how-does-march-native-affect-floating-point-accuracy
         nixpkgs.overlays = [
-          (
-            final: prev:
-            let
-              override =
-                libreoffice:
-                libreoffice.override (prev: {
-                  unwrapped = prev.unwrapped.overrideAttrs (prev: {
-                    patches = prev.patches ++ [
-                      (pkgs.writeText "skip-testArrayFormulasFODS.patch" /* diff */ ''
-                        diff --git a/sc/qa/unit/functions_array.cxx b/sc/qa/unit/functions_array.cxx
-                        index ef0da39f5..43caa9002 100644
-                        --- a/sc/qa/unit/functions_array.cxx
-                        +++ b/sc/qa/unit/functions_array.cxx
-                        @@ -25,6 +25,7 @@ void ArrayFunctionsTest::testArrayFormulasFODS()
-
-                         void ArrayFunctionsTest::testDubiousArrayFormulasFODS()
-                         {
-                        +    return; // flaky https://github.com/NixOS/nixpkgs/issues/398633
-                             //TODO: sc/qa/unit/data/functions/array/dubious/fods/linest.fods produces widely different
-                             // values when built with -ffp-contract enabled (-ffp-contract=on default on Clang 14,
-                             // -ffp-contract=fast default when building with optimizations on GCC) on at least aarch64
-                      '')
-                    ];
-                  });
-                });
-            in
-            {
-              libreoffice-still = override prev.libreoffice-still;
-              libreoffice-qt-still = override prev.libreoffice-qt-still;
-              libreoffice-fresh = override prev.libreoffice-still;
-              libreoffice-qt-fresh = override prev.libreoffice-qt-still;
-              libreoffice-collabora = override prev.libreoffice-collabora;
-            }
-          )
           (final: prev: {
             assimp = prev.assimp.overrideAttrs { doCheck = false; }; # https://github.com/assimp/assimp/issues/6342
           })
