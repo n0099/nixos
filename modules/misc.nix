e3788dba9adb18ed53c1a2ce20da98325acf40d3
n0099 <git@n0099.com>
2026-06-18 21:12:08 +0000

    - re-enable testing for pkg `cryptonite` as `verify sig` of `Ed448` no longer fails: https://github.com/kazu-yamamoto/crypton/issues/49 https://github.com/flatironinstitute/nixpack/commit/5c1157e21187483185ca99d4a8a3e37b8d0a916e, partial revert 9954d64ea53b3066e95b136af9d03afd2b9e4abd - remove substituter https://devenv.cachix.org in `nix.conf` as `devenv` will use it automatically @ modules/misc.nix

--- modules/misc.nix
+++ modules/misc.nix
@@ -43,20 +43,6 @@
       }
       {
         environment.systemPackages = [ pkgs.devenv ];
-        nixpkgs.overlays = [
-          (final: prev: {
-            haskellPackages = prev.haskellPackages.extend (
-              # https://github.com/NixOS/nixpkgs/issues/26561
-              final: prev: {
-                cryptonite = prev.cryptonite.overrideAttrs { doCheck = false; }; # test fails at `verify sig` of `Ed448`: https://github.com/haskell-crypto/cryptonite/blob/d163f69512a3d162baa69a95927f3d6369833f7d/tests/KAT_Ed448.hs#L89
-              }
-            );
-          })
-        ];
-        nix.settings = {
-          extra-substituters = [ "https://devenv.cachix.org" ];
-          extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-        };
       }
     ];
 }
