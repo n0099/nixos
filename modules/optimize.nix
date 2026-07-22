ed48ad0d0d4544b6a79cb317d05123d281d8f4b7
n0099 <git@n0099.com>
2026-07-22 11:26:17 +0000

    * not using pipe operator for `lib.mkIf` to reduce nesting level * re-order config parts in `lib.mkMerge` to match with options @ modules/optimize.nix

--- modules/optimize.nix
+++ modules/optimize.nix
@@ -15,48 +15,42 @@
         };
       };
       config = lib.mkMerge [
-        (
-          {
-            # https://wiki.nixos.org/wiki/Build_flags#Building_the_whole_system_on_NixOS
-            # https://discourse.nixos.org/t/pre-rfc-gradual-transition-of-nixos-x86-64-baseline-to-x86-64-v3-with-an-intermediate-step-to-x86-64-v2/35924
-            # https://wiki.gentoo.org/wiki/GCC_optimization#-march
-            # https://news.ycombinator.com/item?id=45758392
-            # https://wiki.debian.org/ArchitectureVariants
-            nixpkgs.hostPlatform = {
-              gcc = {
-                inherit (cfg) arch;
-                tune = cfg.arch;
-              };
-              system = "x86_64-linux";
+        (lib.mkIf cfg.stdenv {
+          # https://wiki.gentoo.org/wiki/LTO
+          # https://old.reddit.com/r/NixOS/comments/146wdfk/lto_by_default/
+          # https://old.reddit.com/r/NixOS/comments/yww3mr/lto_and_pgo_in_nixos/
+          # https://github.com/ZaMaZaN4iK/awesome-pgo
+          # https://github.com/NixOS/nixpkgs/issues/306953
+          # https://github.com/NixOS/nixpkgs/pull/393458
+          nixpkgs.config.replaceStdenv =
+            { pkgs }:
+            pkgs.stdenvAdapters.overrideCC pkgs.stdenv (
+              # https://github.com/NixOS/nixpkgs/pull/207419
+              pkgs.gcc.override (prev: {
+                # https://github.com/NixOS/nixpkgs/pull/477124
+                cc = prev.cc.override {
+                  reproducibleBuild = false;
+                  profiledCompiler = true;
+                };
+              })
+            );
+        })
+        (lib.mkIf (cfg.arch != "") {
+          # https://wiki.nixos.org/wiki/Build_flags#Building_the_whole_system_on_NixOS
+          # https://discourse.nixos.org/t/pre-rfc-gradual-transition-of-nixos-x86-64-baseline-to-x86-64-v3-with-an-intermediate-step-to-x86-64-v2/35924
+          # https://wiki.gentoo.org/wiki/GCC_optimization#-march
+          # https://news.ycombinator.com/item?id=45758392
+          # https://wiki.debian.org/ArchitectureVariants
+          nixpkgs.hostPlatform = {
+            gcc = {
+              inherit (cfg) arch;
+              tune = cfg.arch;
             };
-            nix.settings.system-features = [ "gccarch-${cfg.arch}" ];
-            nix.settings.keep-outputs = true; # https://discourse.nixos.org/t/rebuild-nixos-offline/3679/16
-          }
-          |> lib.mkIf (cfg.arch != "")
-        )
-        (
-          {
-            # https://wiki.gentoo.org/wiki/LTO
-            # https://old.reddit.com/r/NixOS/comments/146wdfk/lto_by_default/
-            # https://old.reddit.com/r/NixOS/comments/yww3mr/lto_and_pgo_in_nixos/
-            # https://github.com/ZaMaZaN4iK/awesome-pgo
-            # https://github.com/NixOS/nixpkgs/issues/306953
-            # https://github.com/NixOS/nixpkgs/pull/393458
-            nixpkgs.config.replaceStdenv =
-              { pkgs }:
-              pkgs.stdenvAdapters.overrideCC pkgs.stdenv (
-                # https://github.com/NixOS/nixpkgs/pull/207419
-                pkgs.gcc.override (prev: {
-                  # https://github.com/NixOS/nixpkgs/pull/477124
-                  cc = prev.cc.override {
-                    reproducibleBuild = false;
-                    profiledCompiler = true;
-                  };
-                })
-              );
-          }
-          |> lib.mkIf cfg.stdenv
-        )
+            system = "x86_64-linux";
+          };
+          nix.settings.system-features = [ "gccarch-${cfg.arch}" ];
+          nix.settings.keep-outputs = true; # https://discourse.nixos.org/t/rebuild-nixos-offline/3679/16
+        })
       ];
     };
 }
