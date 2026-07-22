474a4322d8fc8ca0751d46e438af0beb649ea09f
n0099 <git@n0099.com>
2026-07-22 11:06:10 +0000

    * merge `stdenv.nix` into `optimize.nix` * move option `n0099.stdenv.enable` to `n0099.optimize.stdenv` @ optimize.nix @ modules

--- modules/optimize.nix
+++ modules/optimize.nix
@@ -7,14 +7,15 @@
     in
     {
       options.n0099.optimize = {
+        stdenv = lib.mkEnableOption "";
         arch = lib.mkOption {
           # https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html#index-march-14
           type = with lib.types; nullOr str;
           default = null;
         };
       };
-      config =
-        lib.mkMerge [
+      config = lib.mkMerge [
+        (
           {
             # https://wiki.nixos.org/wiki/Build_flags#Building_the_whole_system_on_NixOS
             # https://discourse.nixos.org/t/pre-rfc-gradual-transition-of-nixos-x86-64-baseline-to-x86-64-v3-with-an-intermediate-step-to-x86-64-v2/35924
@@ -29,11 +30,33 @@
               system = "x86_64-linux";
             };
             nix.settings.system-features = [ "gccarch-${cfg.arch}" ];
-          }
-          {
             nix.settings.keep-outputs = true; # https://discourse.nixos.org/t/rebuild-nixos-offline/3679/16
           }
-        ]
-        |> lib.mkIf (cfg.arch != null);
+          |> lib.mkIf (cfg.arch != "")
+        )
+        (
+          {
+            # https://wiki.gentoo.org/wiki/LTO
+            # https://old.reddit.com/r/NixOS/comments/146wdfk/lto_by_default/
+            # https://old.reddit.com/r/NixOS/comments/yww3mr/lto_and_pgo_in_nixos/
+            # https://github.com/ZaMaZaN4iK/awesome-pgo
+            # https://github.com/NixOS/nixpkgs/issues/306953
+            # https://github.com/NixOS/nixpkgs/pull/393458
+            nixpkgs.config.replaceStdenv =
+              { pkgs }:
+              pkgs.stdenvAdapters.overrideCC pkgs.stdenv (
+                # https://github.com/NixOS/nixpkgs/pull/207419
+                pkgs.gcc.override (prev: {
+                  # https://github.com/NixOS/nixpkgs/pull/477124
+                  cc = prev.cc.override {
+                    reproducibleBuild = false;
+                    profiledCompiler = true;
+                  };
+                })
+              );
+          }
+          |> lib.mkIf cfg.stdenv
+        )
+      ];
     };
 }
