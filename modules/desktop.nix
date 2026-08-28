e46684bd1ceb55c2fd2c3f3ae0a55985bb41b921
n0099 <git@n0099.com>
2026-08-28 20:03:46 +0000

    * update NVIDIA driver to `610.57.04` * replace xargs in the one-linear shell to fetch the latest NVIDIA driver with GNU Parallel @ modules/desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -26,12 +26,19 @@
           open = true;
           package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
             # https://github.com/NixOS/nixpkgs/issues/416023#issuecomment-3109657947
-            # $ version=xxx.xx.xx sh -xc 'nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run && echo open-gpu-kernel-modules nvidia-{settings,persistenced} | tr " " "\n" | xargs -I{} , nix-prefetch-github NVIDIA {} --rev $version'
-            version = "610.43.02";
-            sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
-            openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
-            settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
-            persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+            # $ version=xxx.xx.xx sh -xc 'nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-$version.run && parallel nix-prefetch-github NVIDIA {} --rev $version ::: open-gpu-kernel-modules nvidia-{settings,persistenced}'
+            version = "610.57.04";
+            sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+            openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
+            settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
+            persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
+            patchesOpen = [
+              (pkgs.fetchpatch {
+                # https://github.com/NVIDIA/open-gpu-kernel-modules/pull/1288
+                url = "https://github.com/NVIDIA/open-gpu-kernel-modules/pull/1288.patch";
+                hash = "sha256-T95DwOaCmEbuOp/5CYqveshrxwhMcantXZ+hRJ7XucA=";
+              })
+            ];
           };
           powerManagement.enable = true; # https://wiki.nixos.org/wiki/NVIDIA#Graphical_corruption_and_system_crashes_on_suspend/resume
         };
