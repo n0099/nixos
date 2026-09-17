376d51cbc4ce45733b5bbf068e369b006e18b9c9
n0099 <git@n0099.com>
2026-09-17 19:45:41 +0000

    * replace the flake url of `nixpkgs` from GitHub to channel tarball: https://discourse.nixos.org/t/psa-use-nixos-org-tarballs-for-your-flake-inputs/79950, this also enables `programs.command-not-found`: https://github.com/NixOS/nixpkgs/pull/489532 * rename the GitHub username in flake url `github:vic/*` to `denful`: https://github.com/denful/import-tree/pull/29 @ inputs.nix

--- inputs.nix
+++ inputs.nix
@@ -1,8 +1,8 @@
 {
   flake-file.inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-file.url = "github:vic/flake-file";
-    import-tree.url = "github:vic/import-tree";
+    flake-file.url = "github:denful/flake-file";
+    import-tree.url = "github:denful/import-tree";
   };
 }
