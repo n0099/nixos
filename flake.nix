41e0587b33890876be37c2bbb29bee9bab3e73ed
n0099 <git@n0099.com>
2026-09-17 19:23:41 +0000

    $ nix run .#write-flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# DO-NOT-EDIT. This file was auto-generated using github:denful/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
   outputs = inputs: import ./outputs.nix inputs;
@@ -19,19 +19,26 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
-    detsys-nix.url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
-    flake-file.url = "github:vic/flake-file";
+    detsys-nix = {
+      url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-file.url = "github:denful/flake-file";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
     nix-cachyos-kernel = {
       url = "github:xddxdd/nix-cachyos-kernel";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
     nixvim.url = "github:nix-community/nixvim/nixos-26.05";
   };
 }
